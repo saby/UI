b2bf26f79b93fa774b43970a30d1a764f74d3856
dv.zuev <dv.zuev@tensor.ru>
2018-11-08 16:09:43 +0300

    Base structure

---
+++ bin/compile-ts.cmd
@@ -0,0 +1,3 @@
+node -v
+call npm install
+node ./node_modules/typescript/lib/tsc -p ./tsconfig.json
---
+++ spec/core.d.ts
@@ -0,0 +1,34 @@
+declare type HashMap<T> = {
+    [propName: string]: T
+};
+
+// Сломает переходы в IDE по ctrl+click
+declare module "json!*" {
+    const json: object;
+    export = json;
+}
+declare module "tmpl!*" {
+    const tmpl: (...args: Array<any>) => string;
+    export = tmpl;
+}
+/*declare module "wml!*" {
+    const tmpl: (...args: Array<any>) => string;
+    export = tmpl;
+}*/
+
+declare const process: any;
+declare const require: any;
+
+declare module "Core/Control" {
+   class Control {
+       public _options: any;
+       constructor(cfg:any);
+       _notify(...args): any;
+      _beforeUpdate(...args): any;
+      _forceUpdate(...args): any;
+
+   }
+   export = Control;
+}
+
+declare let rk: (key: string, ctx?: string | number, num?: number) => string;
