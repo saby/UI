7ff8c3355359625c93bc28af5379bb078c31610f
dv.zuev <dv.zuev@tensor.ru>
2019-01-15 17:47:03 +0300

    base control for application

--- UI/Base.ts
+++ UI/Base.ts
@@ -1,8 +1,10 @@
 /// <amd-module name="UI/Index" />
 import Control from './_base/Control';
 import HTML from './_base/HTML';
+import Document from './_base/Document';
 
 export = {
    Control,
-   HTML
+   HTML,
+   Document
 };
