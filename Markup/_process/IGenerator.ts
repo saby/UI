19401fa8321444857b5ef61e5a5968e34b55aa90
dv.zuev <dv.zuev@tensor.ru>
2019-04-01 10:04:47 +0300

    New builder https://online.sbis.ru/opendoc.html?guid=b4ed11ba-1e4f-4076-986e-378d2ffce013

--- Markup/_process/IGenerator.ts
+++ Markup/_process/IGenerator.ts
@@ -3,7 +3,7 @@ import { IStructureCreator } from "./IStructureCreator";
 export interface IGenerator {
     getMemoData: (rootKey: string, callback:()=>any, deps:Array<any>) => any;
     createText: (text:string, key:string) => any;
-    createTag: (tag:string, options: any, attributes: any, key: string, depsLocal:any, control: any) => any;
+    createTag: (tag:string|Function, options: any, attributes: any, key: string, control: any) => any;
     createControl: (tag:string, options: any, attributes: any, key: string, depsLocal:any, logicParent: any) => any;
     joinElements: () => any;
     setImplementation: (struct:IStructureCreator) => void;
