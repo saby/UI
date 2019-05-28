c2a3a8b8d2e8efc3ab9e09f087afef66e95f02be
mitin <mitin@tensor.ru>
2019-05-28 17:24:40 +0300

    поправил скрипт

--- tests/add_ui_demo_s3mod.py
+++ tests/add_ui_demo_s3mod.py
@@ -3,13 +3,19 @@
 import codecs
 
 sourceText = '<items>'
-module_txt = '<ui_module id="85db6f66-683e-4ab1-9531-cfa9a718f696" name="UIDemo"           url="./../../../../ui/UIDemo/UIDemo.s3mod"/>'
+module_txt = '<ui_module id="85db6f66-683e-4ab1-9531-cfa9a718f696" name="UIDemo"                  url="./../../../../ui/UIDemo/UIDemo.s3mod"/>'
 filename = 'InTestUI.s3srv'
 
 with codecs.open(filename, 'r', 'utf-8') as f:
-    old_txt = f.readlines()
+    txt = f.readlines()
 
-new_txt = old_txt.replace(sourceText, sourceText+'\n    '+module_txt+'\n')
+count = -1
+for tmp_line in txt:
+    count += 1
+    if sourceText in tmp_line:
+        # замена для теста автодополнения связанного с двумя браузерами
+        txt[count] = '    {0}\n        {1}\n'.format(sourceText, module_txt)
+        break
 
 with codecs.open(filename, 'w', 'utf-8') as f:
-    f.writelines(new_txt)
+    f.writelines(txt)
