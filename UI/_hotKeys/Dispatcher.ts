fcd289b11ebcd5183ba88f483d37181253c997a7
aa.shipin <aa.shipin@tensor.ru>
2019-06-19 19:52:23 +0300

    https://online.sbis.ru/opendoc.html?guid=a083e830-8947-40ae-9998-a0161a5cf5a3

--- UI/_hotKeys/Dispatcher.ts
+++ UI/_hotKeys/Dispatcher.ts
@@ -13,51 +13,52 @@ import { constants } from 'Env/Env';
  * Облатсь содержимого body также выделена контролом UI/HotKeys:Dispatcher
  */
 class Dispatcher extends Control {
-    keyDownHandler(event: Event): void {
-        const nativeEvent = event.nativeEvent;
-        const key = 'which' in nativeEvent ? nativeEvent.which : nativeEvent.keyCode;
+   keyDownHandler(event: Event): void {
+      const nativeEvent = event.nativeEvent;
+      const key = 'which' in nativeEvent ? nativeEvent.which : nativeEvent.keyCode;
 
-        // клавиша таб не может быть клавишей по умолчанию, у нее есть конкретное предназначение - переход по табу
-        if (key === constants.key.tab) {
-            return;
-        }
+      // клавиша таб не может быть клавишей по умолчанию, у нее есть конкретное предназначение - переход по табу
+      if (key === constants.key.tab) {
+         return;
+      }
 
-        let needStop = false;
-        // если isTrusted = false, значит это мы запустили событие по горячим клавишам,
-        // его не надо повторно обрабатывать
-        if (event.nativeEvent.isTrusted) {
-            // ищем только в пределах попапа
-            // todo придумать проверку получше https://online.sbis.ru/opendoc.html?guid=50215de6-da5c-44bf-b6f6-a9f7cb0e17d2
-            const wholeParents = goUpByControlTree(nativeEvent.target);
-            const popupIndex = wholeParents.findIndex((parent) => parent._moduleName === 'Controls/_popup/Manager/Popup');
-            const parents = popupIndex === -1 ? wholeParents : wholeParents.slice(0, popupIndex + 1);
-            // Dispatcher и активный контрол должны находиться в одной области. Иначе может отработать действие из
-            // другой области. Например, фокус находится в панели, нажимаем клавишу вниз и в реестре сдвигается маркер
-            if (parents.indexOf(this) !== -1) {
-               for (let i = 0; i < parents.length; i++) {
-                  const parent = parents[i];
-                  if (parent._$defaultActions && parent._$defaultActions[key]) {
-                     parent._$defaultActions[key].action();
-                     needStop = true;
-                     break;
-                  }
+      let needStop = false;
+      // если isTrusted = false, значит это мы запустили событие по горячим клавишам,
+      // его не надо повторно обрабатывать
+      if (event.nativeEvent.isTrusted) {
+         // ищем только в пределах попапа
+         // todo придумать проверку получше https://online.sbis.ru/opendoc.html?guid=50215de6-da5c-44bf-b6f6-a9f7cb0e17d2
+         const wholeParents = goUpByControlTree(nativeEvent.target);
+         const popupIndex = wholeParents.findIndex((parent) => parent._moduleName === 'Controls/_popup/Manager/Popup');
+         const parents = popupIndex === -1 ? wholeParents : wholeParents.slice(0, popupIndex + 1);
+         // Dispatcher и активный контрол должны находиться в одной области. Иначе может отработать действие из
+         // другой области. Например, фокус находится в панели, нажимаем клавишу вниз и в реестре сдвигается маркер
+         if (parents.indexOf(this) !== -1) {
+            for (let i = 0; i < parents.length; i++) {
+               const parent = parents[i];
+               if (parent._$defaultActions && parent._$defaultActions[key]) {
+                  parent._$defaultActions[key].action();
+                  needStop = true;
+                  break;
                }
             }
+         }
 
-        // если диспетчер нашел зарегистрированное действие на сочетание клавиш и запустил обработчик,
-        // клавиши считаются обработанными и больше не должны всплывтаь
-        if (needStop) {
-           // Так как наша система событий ловит события на стадии capture,
-           // а подписки в БТРе на стадии bubbling, то не нужно звать stopPropagation
-           // так как обработчики БТРа в таком случае не отработают, потому что
-           // у события не будет bubbling фазы
-           // TODO: Нужно поправить после исправления
-           // https://online.sbis.ru/opendoc.html?guid=cefa8cd9-6a81-47cf-b642-068f9b3898b7
-           if (!event.target.closest('.richEditor_TinyMCE') && !event.target.closest('.controls-RichEditor')) {
-              event.stopPropagation();
-           }
-        }
-    }
+         // если диспетчер нашел зарегистрированное действие на сочетание клавиш и запустил обработчик,
+         // клавиши считаются обработанными и больше не должны всплывтаь
+         if (needStop) {
+            // Так как наша система событий ловит события на стадии capture,
+            // а подписки в БТРе на стадии bubbling, то не нужно звать stopPropagation
+            // так как обработчики БТРа в таком случае не отработают, потому что
+            // у события не будет bubbling фазы
+            // TODO: Нужно поправить после исправления
+            // https://online.sbis.ru/opendoc.html?guid=cefa8cd9-6a81-47cf-b642-068f9b3898b7
+            if (!event.target.closest('.richEditor_TinyMCE') && !event.target.closest('.controls-RichEditor')) {
+               event.stopPropagation();
+            }
+         }
+      }
+   }
 }
 
 // @ts-ignore
