bf481509fd59e0cb60f9e66dcde37f76b362fd03
aa.shipin <aa.shipin@tensor.ru>
2019-06-30 13:34:33 +0300

    Revert "https://online.sbis.ru/opendoc.html?guid=6d45207e-87c6-4d38-a3c0-adc1a8377d6c"
    This reverts commit b4c48bace69febaa576ec10a700da573371ba64b.

--- UI/_base/Control.ts
+++ UI/_base/Control.ts
@@ -186,7 +186,8 @@ export default class Control<TOptions extends IControlOptions = {}, TState = voi
                     // This is fix for specific case. When environment has _haveRebuildRequest and after that 
                     // we creating another one. We don't have to do that, it's better to delay rebuild, after current
                     // sync cycle.
-                    if (environment._haveRebuildRequest) {
+                    // after 410 condition "control._moduleName === 'FED2/UI/DocumentCompatible'" will be deleted.
+                    if (environment._haveRebuildRequest && control._moduleName === 'FED2/UI/DocumentCompatible') {
                         control._$needForceUpdate = true;
                     } else {
                         environment.forceRebuild(controlNode.id);
