1583a4c592412987694f1148e091c48f85f08ec0
aa.shipin <aa.shipin@tensor.ru>
2019-07-02 13:05:24 +0300

    https://online.sbis.ru/opendoc.html?guid=125a6ee8-7bac-4129-87db-8ff278c603bd

--- UI/_hotKeys/KeyStop.ts
+++ UI/_hotKeys/KeyStop.ts
@@ -1,20 +1,30 @@
-// @ts-ignore
-import { Control } from 'UI/Base';
-// @ts-ignore
-import template = require('wml!UI/_hotKeys/KeyStop');
-
+import { Control, IControlOptions, TemplateFunction } from 'UI/Base';
+import * as template from 'wml!UI/_hotKeys/KeyStop';
 import { SyntheticEvent } from 'Vdom/Vdom';
 
-class KeyStop extends Control {
-   _keydownHandler(event: SyntheticEvent): void {
+interface IKeyStopItem {
+   keyCode: number;
+}
+
+interface IKeyStopOptions extends IControlOptions {
+   stopKeys: IKeyStopItem[];
+}
+
+/**
+ * Контрол, который позволяет предотвращать всплытие событий нажатий на клавиши
+ * @class UI/_hotKeys/KeyStop
+ * @extends UI/Base:Control
+ * @public
+ * @author Шипин А.А.
+ */
+export default class KeyStop extends Control<IKeyStopOptions> {
+   protected _template: TemplateFunction = template;
+
+   private _keydownHandler(event: SyntheticEvent<KeyboardEvent>): void {
       const keys = this._options.stopKeys || [];
-      if (keys.find((key) => key.keyCode === event.keyCode)) {
+
+      if (keys.find((key) => key.keyCode === event.nativeEvent.keyCode)) {
          event.stopped = true;
       }
    }
 }
-
-// @ts-ignore
-KeyStop.prototype._template = template;
-
-export default KeyStop;
