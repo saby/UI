469ca9419e84474c9cee6fa78aacde2c5d435ee2
aa.bykanov <aa.bykanov@tensor.ru>
2019-07-31 13:06:50 +0300

    Изменяю положение опций в файле.

--- UI/_base/Control.ts
+++ UI/_base/Control.ts
@@ -324,106 +324,7 @@ export default class Control<TOptions extends IControlOptions = {}, TState = voi
     _forceUpdate(): void {
         // будет переопределено в конструкторе, чтобы был доступ к замыканиям
     }
-
-    /**
-     * @name UI/_base/Control#readOnly
-     * @cfg {Boolean} Определяет, может ли пользователь изменить значение контрола.
-     * (или взаимодействовать с контролом, если его значение не редактируется).
-     * @variant true Пользователь не может изменить значение контрола. (или взаимодействовать с контролом, если его значение не редактируется).
-     * @variant false  Пользователь может изменить значение контрола. (или взаимодействовать с контролом, если его значение не редактируется).
-     * @variant inherited Значение контрола унаследовано от родителя.
-     * @default Inherited
-     * @example
-     * Рассмотрим на примере контролов List и Input. Текст будет отображаться со стилем "только для чтения", и пользователь не сможет его редактировать. 
-     * Однако, у кнопки есть опция readOnly, которая имеет значение false, поэтому кнопка не унаследует эту опцию из списка, и пользователь сможет кликнуть по ней. 
-     * <pre>
-     *    <Controls.list:View readOnly="{{true}}">
-     *       <ws:itemTemplate>
-     *          <Controls.input:Text />
-     *          <Controls.buttons:Path readOnly="{{false}}" />
-     *       </ws:itemTemplate>
-     *    </Controls.list:View>
-     * </pre>
-     * @remark Эта опция наследуется. Если параметр не задан явно, значение параметра наследуется от родительского контрола. По умолчанию все контролы активны.
-     * @see Inherited options
-     */
-
-    /*
-     * @name UI/_base/Control#readOnly
-     * @cfg {Boolean} Determines whether user can change control's value
-     * (or interact with the control if its value is not editable).
-     * @variant true User cannot change control's value (or interact with the control if its value is not editable).
-     * @variant false User can change control's value (or interact with the control if its value is not editable).
-     * @variant inherited Value inherited from the parent.
-     * @default Inherited
-     * @example
-     * In this example, List and Input.Text will be rendered with read-only styles, and the user won't be
-     * able to edit them. However, Button has readOnly option explicitly set to false,
-     * thus it won't inherit this option from the List, and user will be able to click on it.
-     * <pre>
-     *    <Controls.list:View readOnly="{{true}}">
-     *       <ws:itemTemplate>
-     *          <Controls.input:Text />
-     *          <Controls.buttons:Path readOnly="{{false}}" />
-     *       </ws:itemTemplate>
-     *    </Controls.list:View>
-     * </pre>
-     * @remark This option is inherited. If option is not set explicitly, option's value will be inherited
-     * from the parent control. By default, all controls are active.
-     * @see Inherited options
-     */
-
-    /**
-     * @name UI/_base/Control#theme
-     * @cfg {String} Название темы. В зависимости от темы загружаются различные таблицы стилей и применяются различные стили к контролу.
-     * @variant any Любое значение, переданное контролу.
-     * @variant inherited Значение, унаследованное от родителя.
-     * @default ''(пустая строка)
-     * @example
-     * В этом примере Controls.Application и все его дочерние контролы будут иметь стиль темы "carry". 
-     * Однако, Carry.Head будет иметь стиль "carry". 
-     * Если вы поместите контролы в Carry.Head и не укажите опцию theme, они унаследуют тему "carry".
-     * <pre>
-     *    <Controls.Application theme="carry">
-     *       <Carry.Head theme="presto" />
-     *       <Carry.Workspace>
-     *          <Controls.Tree />
-     *       </Carry.Workspace>
-     *    </Controls.Application>
-     * </pre>
-     * @remark Эта опция наследуется. Если параметр не задан явно, значение параметра наследуется от родительского контрола.
-     * Путь к CSS-файлу с параметрами темы определяется автоматически на основе имени темы.
-     * CSS-файлы должны быть подготовлены заранее в соответствии с документацией.
-     * @see Themes
-     * @see Inherited options
-     */
-
-    /*
-     * @name UI/_base/Control#theme
-     * @cfg {String} Theme name. Depending on the theme, different stylesheets are loaded and
-     * different styles are applied to the control.
-     * @variant any Any value that was passed to the control.
-     * @variant inherited Value inherited from the parent.
-     * @default ''(empty string)
-     * @example
-     * In this example, Controls.Application and all of its chil controls will have "carry" theme styles.
-     * However, Carry.Head will "carry" theme styles. If you put controls inside Carry.Head and does not specify
-     * the theme option, they will inherit "carry" theme.
-     * <pre>
-     *    <Controls.Application theme="carry">
-     *       <Carry.Head theme="presto" />
-     *       <Carry.Workspace>
-     *          <Controls.Tree />
-     *       </Carry.Workspace>
-     *    </Controls.Application>
-     * </pre>
-     * @remark This option is inherited. If option is not set explicitly, option's value will be inherited
-     * from the parent control. The path to CSS file with theme parameters determined automatically
-     * based on the theme name. CSS files should be prepared in advance according to documentation.
-     * @see Themes
-     * @see Inherited options
-     */
-
+    
     getInstanceId(): string {
         return this._instId;
     }
@@ -1217,3 +1118,102 @@ export default class Control<TOptions extends IControlOptions = {}, TState = voi
 
 // @ts-ignore
 Control.prototype._template = template;
+
+    /**
+     * @name UI/_base/Control#readOnly
+     * @cfg {Boolean} Определяет, может ли пользователь изменить значение контрола.
+     * (или взаимодействовать с контролом, если его значение не редактируется).
+     * @variant true Пользователь не может изменить значение контрола. (или взаимодействовать с контролом, если его значение не редактируется).
+     * @variant false  Пользователь может изменить значение контрола. (или взаимодействовать с контролом, если его значение не редактируется).
+     * @variant inherited Значение контрола унаследовано от родителя.
+     * @default Inherited
+     * @example
+     * Рассмотрим на примере контролов List и Input. Текст будет отображаться со стилем "только для чтения", и пользователь не сможет его редактировать. 
+     * Однако, у кнопки есть опция readOnly, которая имеет значение false, поэтому кнопка не унаследует эту опцию из списка, и пользователь сможет кликнуть по ней. 
+     * <pre>
+     *    <Controls.list:View readOnly="{{true}}">
+     *       <ws:itemTemplate>
+     *          <Controls.input:Text />
+     *          <Controls.buttons:Path readOnly="{{false}}" />
+     *       </ws:itemTemplate>
+     *    </Controls.list:View>
+     * </pre>
+     * @remark Эта опция наследуется. Если параметр не задан явно, значение параметра наследуется от родительского контрола. По умолчанию все контролы активны.
+     * @see Inherited options
+     */
+
+    /*
+     * @name UI/_base/Control#readOnly
+     * @cfg {Boolean} Determines whether user can change control's value
+     * (or interact with the control if its value is not editable).
+     * @variant true User cannot change control's value (or interact with the control if its value is not editable).
+     * @variant false User can change control's value (or interact with the control if its value is not editable).
+     * @variant inherited Value inherited from the parent.
+     * @default Inherited
+     * @example
+     * In this example, List and Input.Text will be rendered with read-only styles, and the user won't be
+     * able to edit them. However, Button has readOnly option explicitly set to false,
+     * thus it won't inherit this option from the List, and user will be able to click on it.
+     * <pre>
+     *    <Controls.list:View readOnly="{{true}}">
+     *       <ws:itemTemplate>
+     *          <Controls.input:Text />
+     *          <Controls.buttons:Path readOnly="{{false}}" />
+     *       </ws:itemTemplate>
+     *    </Controls.list:View>
+     * </pre>
+     * @remark This option is inherited. If option is not set explicitly, option's value will be inherited
+     * from the parent control. By default, all controls are active.
+     * @see Inherited options
+     */
+
+    /**
+     * @name UI/_base/Control#theme
+     * @cfg {String} Название темы. В зависимости от темы загружаются различные таблицы стилей и применяются различные стили к контролу.
+     * @variant any Любое значение, переданное контролу.
+     * @variant inherited Значение, унаследованное от родителя.
+     * @default ''(пустая строка)
+     * @example
+     * В этом примере Controls.Application и все его дочерние контролы будут иметь стиль темы "carry". 
+     * Однако, Carry.Head будет иметь стиль "carry". 
+     * Если вы поместите контролы в Carry.Head и не укажите опцию theme, они унаследуют тему "carry".
+     * <pre>
+     *    <Controls.Application theme="carry">
+     *       <Carry.Head theme="presto" />
+     *       <Carry.Workspace>
+     *          <Controls.Tree />
+     *       </Carry.Workspace>
+     *    </Controls.Application>
+     * </pre>
+     * @remark Эта опция наследуется. Если параметр не задан явно, значение параметра наследуется от родительского контрола.
+     * Путь к CSS-файлу с параметрами темы определяется автоматически на основе имени темы.
+     * CSS-файлы должны быть подготовлены заранее в соответствии с документацией.
+     * @see Themes
+     * @see Inherited options
+     */
+
+    /*
+     * @name UI/_base/Control#theme
+     * @cfg {String} Theme name. Depending on the theme, different stylesheets are loaded and
+     * different styles are applied to the control.
+     * @variant any Any value that was passed to the control.
+     * @variant inherited Value inherited from the parent.
+     * @default ''(empty string)
+     * @example
+     * In this example, Controls.Application and all of its chil controls will have "carry" theme styles.
+     * However, Carry.Head will "carry" theme styles. If you put controls inside Carry.Head and does not specify
+     * the theme option, they will inherit "carry" theme.
+     * <pre>
+     *    <Controls.Application theme="carry">
+     *       <Carry.Head theme="presto" />
+     *       <Carry.Workspace>
+     *          <Controls.Tree />
+     *       </Carry.Workspace>
+     *    </Controls.Application>
+     * </pre>
+     * @remark This option is inherited. If option is not set explicitly, option's value will be inherited
+     * from the parent control. The path to CSS file with theme parameters determined automatically
+     * based on the theme name. CSS files should be prepared in advance according to documentation.
+     * @see Themes
+     * @see Inherited options
+     */
