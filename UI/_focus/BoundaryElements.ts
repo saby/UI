a3a5c6a09f1c6fab48b807f4275e4d6abc30923c
aa.shipin <aa.shipin@tensor.ru>
2019-08-30 18:11:34 +0300

    test

--- UI/_focus/BoundaryElements.ts
+++ UI/_focus/BoundaryElements.ts
@@ -138,12 +138,21 @@ function appendFocusesElements(environment, vnode) {
 }
 
 export function insertBoundaryElements(environment, vnode) {
-   const dom = vnode.dom;
-   if (dom === environment._rootDOMNode && environment._rootDOMNode.tagName !== 'HTML' || vnode.type === 'body') {
-      if (vnode && vnode.children) {
-         var appendedElements = appendFocusesElements(environment, vnode);
-         if (appendedElements) {
+   if (environment._rootDOMNode.tagName !== 'HTML') {
+      if (vnode && appendFocusesElements(environment, vnode)) {
+         if (vnode && appendFocusesElements(environment, vnode)) {
             appendFocusElementsToDOM(environment._rootDOMNode);
+         }
+      }
+   } else {
+      if (vnode && vnode.children && vnode.children[1]) {
+         const body = vnode.children[1];
+         if (vnode && appendFocusesElements(environment, body)) {
+            let bodyDOM = environment._rootDOMNode.getElementsByTagName('body');
+            if (bodyDOM.length) {
+               bodyDOM = bodyDOM[0];
+               appendFocusElementsToDOM(bodyDOM);
+            }
          }
       }
    }
