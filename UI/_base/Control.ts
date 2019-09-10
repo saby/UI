850db055c7b5aeca270f008d37c0fb5660faa25f
aa.shipin <aa.shipin@tensor.ru>
2019-09-10 13:28:44 +0300

    https://online.sbis.ru/opendoc.html?guid=f3dbb3f6-e329-470d-9086-3a6d35423148

--- UI/_base/Control.ts
+++ UI/_base/Control.ts
@@ -101,6 +101,7 @@ export default class Control<TOptions extends IControlOptions = {}, TState = voi
     private _unmounted: boolean = false;
     private _destroyed: boolean = false;
     private _$active: boolean = false;
+    private _reactiveStart: boolean = false;
 
     private readonly _instId: string;
     protected _options: TOptions = null;
@@ -719,6 +720,9 @@ export default class Control<TOptions extends IControlOptions = {}, TState = voi
         ReactiveObserver.observeProperties(this);
 
         let resultBeforeMount = this._beforeMount.apply(this, arguments);
+
+        // _reactiveStart means starting of monitor change in properties
+        this._reactiveStart = true;
 
         if (typeof window === 'undefined') {
             if (resultBeforeMount && resultBeforeMount.callback) {
