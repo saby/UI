2c3e85b9ce0bcb84f2a2d14571cf14c56c7f9c5a
aa.ibragimov <aa.ibragimov@tensor.ru>
2020-03-17 11:19:11 +0300

    https://online.sbis.ru/opendoc.html?guid=d436db28-f4e4-4fd6-a0c1-f2b77864d262

--- UIDemo/ThemesDemo/Page.ts
+++ UIDemo/ThemesDemo/Page.ts
@@ -4,14 +4,14 @@ import { Control } from 'UI/Base';
 // @ts-ignore
 import template = require('wml!UIDemo/ThemesDemo/Page');
 
-const LIGTH_THEME = 'default__light';
+const LIGHT_THEME = 'default__light';
 const DARK_THEME = 'default__dark';
-const invertTheme = (theme: string) => (theme === LIGTH_THEME) ? DARK_THEME : LIGTH_THEME;
+const invertTheme = (theme: string) => (theme === LIGHT_THEME) ? DARK_THEME : LIGHT_THEME;
 
 export default class Page extends Control {
    _template = template;
-   theme1 = LIGTH_THEME;
-   theme2 = LIGTH_THEME;
+   theme1 = LIGHT_THEME;
+   theme2 = LIGHT_THEME;
    switchFirst() {
       this.theme1 = invertTheme(this.theme1);
    }
