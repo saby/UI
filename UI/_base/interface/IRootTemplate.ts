aec8dd51f7ee66cfe02012caa8f5d83d72f4b08c
aa.bykanov <aa.bykanov@tensor.ru>
2020-04-13 12:53:48 +0300

    Опубликованы 2 интерфейса https://online.sbis.ru/opendoc.html?guid=045a35b1-fbb1-4837-859e-dc486d8d42e9

--- UI/_base/interface/IRootTemplate.ts
+++ UI/_base/interface/IRootTemplate.ts
@@ -1,46 +1,49 @@
 /**
+ * Интерфейс для контролов, поддерживающих конфигурацию <a href="/doc/platform/developmentapl/middleware/preprocessor/">сервиса представления</a> приложения.
  * @interface UI/_base/interface/IRootTemplate
-*/
+ * @public
+ * @author Шипин А.А.
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#buildnumber
  * @cfg {string} Номер сборки. Прибавляется ко всем url при запросах к серверу.
  * Используется при кешировании. Если меняется номер сборки,
  * все файлы начинают тянуться с сервера, а не из кеша браузера, потому что у них становится другой url.
-*/
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#product
  * @cfg {string}
-*/
+ */
 
 /** 
  * @name UI/_base/interface/IRootTemplate#wsRoot
  * @cfg {String} Путь к корню интерфейсного модуля WS.Core. Например, "/resources/WS.Core/".
  * @remark
  * Значение опции задаётся относительно URL-адреса сервиса.
- * URL-адрес сервиса устанавливается через <a href="https://wi.sbis.ru/doc/platform/developmentapl/middleware/cloud-control/">Сервис управления облаком</a> в разделе "Структура облака".
+ * URL-адрес сервиса устанавливается через <a href="/doc/platform/developmentapl/middleware/cloud-control/">Сервис управления облаком</a> в разделе "Структура облака".
  * Данная настройка попадает в свойство wsRoot объекта window.wsConfig.
-*/
+ */
 
 /** 
  * @name UI/_base/interface/IRootTemplate#wsRoot
  * @cfg {String} Path to ws root url
-*/
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#resourceRoot
  * @cfg {String} Адрес к директории с ресурсами сервиса. Например, "/resources/".
  * @remark
  * Значение опции задаётся относительно URL-адреса сервиса.
- * URL-адрес сервиса устанавливается через <a href="https://wi.sbis.ru/doc/platform/developmentapl/middleware/cloud-control/">Сервис управления облаком</a> в разделе "Структура облака".
+ * URL-адрес сервиса устанавливается через <a href="/doc/platform/developmentapl/middleware/cloud-control/">Сервис управления облаком</a> в разделе "Структура облака".
  * Данная настройка попадает в свойство resourceRoot объекта window.wsConfig.
-*/
+ */
 
 /** 
  * @name UI/_base/interface/IRootTemplate#resourceRoot
  * @cfg {String} Path to resource root url
-*/
+ */
 
 
 /**
@@ -48,26 +51,26 @@
  * @cfg {String} Адрес к директории сервиса. Например, "/".
  * @remark
  * Значение опции задаётся относительно URL-адреса сервиса.
- * URL-адрес сервиса устанавливается через <a href="https://wi.sbis.ru/doc/platform/developmentapl/middleware/cloud-control/">Сервис управления облаком</a> в разделе "Структура облака".
+ * URL-адрес сервиса устанавливается через <a href="/doc/platform/developmentapl/middleware/cloud-control/">Сервис управления облаком</a> в разделе "Структура облака".
  * Данная настройка попадает в свойство appRoot объекта window.wsConfig.
-*/
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#appRoot
  * @cfg {String} Path to application root url
-*/
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#staticDomains
  * @cfg {Array} Список, содержащий набор доменов для загрузки статики.
  * Список доменов решает задачу загрузки статических ресурсов с нескольких документов. Эти домены будут использоваться для создания путей для статических ресурсов и распределения загрузки для нескольких статических доменов.
-*/
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#staticDomains
  * @cfg {Array} The list of domains for distributing static resources. These domains will be used to create paths
  * for static resources and distribute downloading for several static domains.
-*/
+ */
 
 /**
  * @name UI/_base/interface/IRootTemplate#servicesPath
