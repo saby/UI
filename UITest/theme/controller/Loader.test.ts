95b33563c64df51f7c6b60a440eb9f9c3a880d8e
aa.ibragimov <aa.ibragimov@tensor.ru>
2020-05-28 11:41:36 +0300

    https://online.sbis.ru/opendoc.html?guid=11492f92-5991-478a-84d0-b83ab7bcf181

--- UITest/theme/controller/Loader.test.ts
+++ UITest/theme/controller/Loader.test.ts
@@ -6,29 +6,30 @@ import { EMPTY_THEME } from 'UI/theme/_controller/css/const';
 describe('UI/theme/_controller/Loader', () => {
    const loader = new Loader();
    const theme_name = 'themeName';
-   const first_name = 'Root'
+   const first_name = 'Root';
    const last_name = 'control';
    const mod = 'modificaton';
    describe('getHref', () => {
       it('Не меняет путь, содержащий .css (загрузка rt-пакетов)', () => {
          const link = 'some/style.css';
-         assert.strictEqual(loader.getHref(link, theme_name), link);
+         assert.include(
+            loader.getHref(link, theme_name), link);
       });
 
       it('Разрешает путь до темизированных стилей', () => {
-         assert.strictEqual(
-            loader.getHref(`${first_name}/${last_name}`, theme_name),
+         assert.include(
+            loader.getHref(`${first_name}/${last_name}`, theme_name), 
             `${first_name}-${theme_name}-theme/${last_name}.css`
          );
       });
       it('Разрешает путь до темизированных стилей с модификацией', () => {
-         assert.strictEqual(
+         assert.include(
             loader.getHref(`${first_name}/${last_name}`, `${theme_name}__${mod}`),
             `${first_name}-${theme_name}-theme/${mod}/${last_name}.css`
          );
       });
       it('Разрешает путь до нетемизированных стилей', () => {
-         assert.strictEqual(
+         assert.include(
             loader.getHref(`${first_name}/${last_name}`, EMPTY_THEME),
             `${first_name}/${last_name}.css`
          );
