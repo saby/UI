397034eb8dc806da64961165368034593b3027a2
rn.kondakov <rn.kondakov@tensor.ru>
2020-07-13 16:31:51 +0300

    https://online.sbis.ru/opendoc.html?guid=f939c883-f167-4527-b0e3-d0b0ca10ce32

--- UI/_base/HTML.ts
+++ UI/_base/HTML.ts
@@ -67,6 +67,17 @@ class HTML extends Control<IHTMLCombinedOptions> {
         this.compat = cfg.compat || false;
     }
 
+    private markForeignContent(): void {
+        if (!this.onServer) {
+            const bodyChildren: HTMLCollection = document.body.children;
+            for (let i = 0; i < bodyChildren.length; i++) {
+                if (bodyChildren[i].id !== 'wasaby-content') {
+                    bodyChildren[i].setAttribute('data-vdomignore', 'true');
+                }
+            }
+        }
+    }
+
     /**
      * @mixes UI/_base/HTML/IHTML
      * @mixes UI/_base/HTML/IRootTemplate
@@ -82,6 +93,8 @@ class HTML extends Control<IHTMLCombinedOptions> {
             this.metaStack.push({ title: cfg.title });
         }
         let appData = AppData.getAppData();
+
+        this.markForeignContent();
 
         this.buildnumber = cfg.buildnumber || constants.buildnumber;
 
@@ -152,6 +165,7 @@ class HTML extends Control<IHTMLCombinedOptions> {
     }
 
     _beforeUpdate(options: IHTMLCombinedOptions): void {
+        this.markForeignContent();
         if (options.title !== this._options.title) {
             const prevState = this.metaStack.lastState;
             this.metaStack.push({ title: options.title });
