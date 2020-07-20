eaab2d0b711024816cc74d87bcbbb1c0a8dbf56b
rn.kondakov <rn.kondakov@tensor.ru>
2020-07-20 17:43:59 +0300

    https://online.sbis.ru/opendoc.html?guid=bd46cbdd-8950-4604-ac34-53c4cca4da88

--- UI/_vdom/Synchronizer/resources/DOMEnvironment.ts
+++ UI/_vdom/Synchronizer/resources/DOMEnvironment.ts
@@ -225,6 +225,8 @@ export default class DOMEnvironment extends QueueMixin implements IDOMEnvironmen
    destroy(): any {
       this.runQueue();
 
+      // @ts-ignore FIXME: Property '_haveRebuildRequest' does not exist
+      this._haveRebuildRequest = false;
       this.removeTabListener();
       // TODO раскомментить после https://online.sbis.ru/opendoc.html?guid=450170bd-6322-4c3c-b6bd-3520ce3cba8a
       // this.removeProcessiingEventHandler('focus');
@@ -241,7 +243,6 @@ export default class DOMEnvironment extends QueueMixin implements IDOMEnvironmen
       this.__captureEventHandlers = {};
       delete this.__captureEventHandler;
       this._handleTabKey = undefined;
-      this._haveRebuildRequest = false;
       super.destroy();
    }
 
