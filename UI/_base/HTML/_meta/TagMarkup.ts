ed725a715131f17ecd08153ac08317975b067e23
aa.ibragimov <aa.ibragimov@tensor.ru>
2020-07-21 18:20:47 +0300

    HTML Void Elements
    https://online.sbis.ru/opendoc.html?guid=c7268b1e-ce5c-4156-bed9-5a0764249fe7

--- UI/_base/HTML/_meta/TagMarkup.ts
+++ UI/_base/HTML/_meta/TagMarkup.ts
@@ -6,6 +6,14 @@ export const DEFAULT_ATTRS = {
    'data-vdomignore': 'true'
 };
 
+// https://www.w3.org/TR/2011/WD-html-markup-20110113/syntax.html#void-element
+const HTML_VOID_ELEMENTS = {
+   'area': true, 'base': true, 'br': true, 'col': true,
+   'command': true, 'embed': true, 'hr': true, 'img': true,
+   'input': true, 'keygen': true, 'link': true, 'meta': true,
+   'param': true, 'source': true, 'track': true, 'wbr': true
+};
+
 export default class {
    outerHTML: string = '';
 
@@ -18,8 +26,11 @@ export default class {
 
 export function generateTagMarkup({ tagName, attrs, children }: ITagDescription = { tagName: 'no_tag', attrs: {} }) {
    const _atts = { ...DEFAULT_ATTRS, ...attrs };
-   const attrMarkup = Object.entries(_atts).map(([key, val]) => `"${key}"="${val}"`).join(' ');
-   if (!children) { return `<${tagName} ${attrMarkup}>`; }
+   const attrMarkup = Object.entries(_atts).map(([key, val]) => `${key}="${val}"`).join(' ');
+   if (!children) {
+      const endChar = HTML_VOID_ELEMENTS[tagName] ? '' : '/';
+      return `<${tagName} ${attrMarkup}${endChar}>`;
+   }
    const childMarkup = (typeof children === 'string') ? children : generateTagMarkup(children);
    return `<${tagName} ${attrMarkup}> ${childMarkup} </${tagName}>`;
 }
--- UITest/_base/HTML/meta/TagMarkup.test.ts
+++ UITest/_base/HTML/meta/TagMarkup.test.ts
@@ -8,25 +8,31 @@ describe('generateTagMarkup', () => {
    it('tag without attrs', () => {
       assert.strictEqual(
          generateTagMarkup({ tagName }),
-         '<tag "data-vdomignore"="true">'
+         '<tag data-vdomignore="true"/>'
+      );
+   });
+   it('void tag without attrs', () => {
+      assert.strictEqual(
+         generateTagMarkup({ tagName: 'meta' }),
+         '<meta data-vdomignore="true">'
       );
    });
    it('tag with attrs', () => {
       assert.strictEqual(
          generateTagMarkup({ tagName, attrs }),
-         '<tag "data-vdomignore"="true" "attr1"="val1" "attr2"="val2">'
+         '<tag data-vdomignore="true" attr1="val1" attr2="val2"/>'
       );
    });
    it('tag with string child', () => {
       assert.deepEqual(
          generateTagMarkup({ tagName, children: 'child' }),
-         '<tag "data-vdomignore"="true"> child </tag>'
+         '<tag data-vdomignore="true"> child </tag>'
       );
    });
    it('tag with attrs & string child', () => {
       assert.deepEqual(
          generateTagMarkup({ tagName, attrs, children: 'child' }),
-         '<tag "data-vdomignore"="true" "attr1"="val1" "attr2"="val2"> child </tag>'
+         '<tag data-vdomignore="true" attr1="val1" attr2="val2"> child </tag>'
       );
    });
 });
