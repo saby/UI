4a5e9fc89408d1026655253d2c86388d01fd17a1
ma.krylov <ma.krylov@tensor.ru>
2020-08-06 09:26:41 +0300

    https://online.sbis.ru/opendoc.html?guid=90d1dad5-3e8a-4c9d-9afc-5ba26c0f3368

--- UI/Builder.ts
+++ UI/Builder.ts
@@ -5,7 +5,7 @@
  */
 
 import * as Tmpl from './_builder/Tmpl';
-import * as Compiler from './_builder/Compiler';
+import { Compiler } from './_builder/Compiler';
 import * as Config from './_builder/config';
 
 export {
