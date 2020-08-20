6472661fe76068db5fe55592ac1fd20743cba6e8
va.ten <va.ten@tensor.ru>
2020-08-20 16:38:51 +0300

    https://online.sbis.ru/opendoc.html?guid=14cf219e-2380-494f-8894-fea46180bf9b

--- UI/_builder/Tmpl/codegen/jstpl.ts
+++ UI/_builder/Tmpl/codegen/jstpl.ts
@@ -26,7 +26,7 @@ export const BIND = `(function (self, _data) {
 export const BODY = `if (typeof forceCompatible === 'undefined') {
     forceCompatible = false;
 }
-var markupGenerator = thelpers.getMarkupGenerator(isVdom, forceCompatible);
+var markupGenerator = thelpers.createGenerator(isVdom, forceCompatible);
 /*#DELETE IT START#*/
 if (typeof includedTemplates === "undefined") {
    eval("var includedTemplates = undefined;");
