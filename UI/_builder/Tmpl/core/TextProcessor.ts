cd6539cce92ba683b630c2f4c86e0015470619d1
ma.krylov <ma.krylov@tensor.ru>
2020-08-25 17:48:19 +0300

    text proc
    https://online.sbis.ru/opendoc.html?guid=e238a42e-fedf-4601-bb71-e77dd475cff6

---
+++ UI/_builder/Tmpl/core/TextProcessor.ts
@@ -0,0 +1,87 @@
+/// <amd-module name="UI/_builder/Tmpl/core/TextProcessor" />
+
+/**
+ * @author Крылов М.А.
+ */
+
+import { splitLocalizationText } from './i18n';
+import {
+   TText,
+   TranslationNode,
+   TextDataNode,
+   ExpressionNode
+} from './Ast';
+import { IParser } from 'UI/_builder/Tmpl/expressions/_private/Parser';
+
+const VARIABLES_PATTERN = /\{\{ ?([\s\S]*?) ?\}\}/g;
+const LOCALIZATION_PATTERN = /\{\[ ?([\s\S]*?) ?\]\}/g;
+
+declare type TWrapper = (data: string) => TText;
+
+function createTranslationNode(data: string): TranslationNode {
+   const { text, context } = splitLocalizationText(data);
+   return new TranslationNode(text, context);
+}
+
+function createTextNode(data: string): TextDataNode {
+   return new TextDataNode(data);
+}
+
+function markDataByRegex(
+   nodes: TText[],
+   regex: RegExp,
+   targetWrapper: TWrapper,
+   defaultWrapper: TWrapper
+): TText[] {
+   let item;
+   let value;
+   let last;
+   const data = [];
+   for (let idx = 0; idx < nodes.length; ++idx) {
+      if (!(nodes[idx] instanceof TextDataNode)) {
+         data.push(nodes[idx]);
+         continue;
+      }
+
+      const stringData = (<TextDataNode>nodes[idx]).__$ws_content;
+
+      regex.lastIndex = 0;
+      last = 0;
+      while ((item = regex.exec(stringData))) {
+         if (last < item.index) {
+            value = stringData.slice(last, item.index);
+            data.push(defaultWrapper(value));
+         }
+         data.push(targetWrapper(item[1]));
+         last = item.index + item[0].length;
+      }
+
+      if (last === 0) {
+         data.push(nodes[idx]);
+      } else if (last < stringData.length) {
+         value = stringData.slice(last);
+         data.push(defaultWrapper(value));
+      }
+   }
+   return data;
+}
+
+export function processTextData(text: string, expressionParser: IParser): TText[] {
+   const processedText = [
+      new TextDataNode(text)
+   ];
+
+   const processedExpressions = markDataByRegex(
+      processedText,
+      VARIABLES_PATTERN,
+      (data: string) => new ExpressionNode(expressionParser.parse(data)),
+      createTextNode
+   );
+
+   return markDataByRegex(
+      processedExpressions,
+      LOCALIZATION_PATTERN,
+      createTranslationNode,
+      createTextNode
+   );
+}
---
+++ UI/_builder/Tmpl/core/i18n.ts
@@ -0,0 +1,47 @@
+/// <amd-module name="UI/_builder/Tmpl/core/i18n" />
+
+/**
+ * @author Крылов М.А.
+ */
+
+const EMPTY_STRING = '';
+
+export function splitLocalizationText(text: string): { text: string, context: string } {
+   const pair = text.split('@@');
+   if (pair.length > 2) {
+      throw new Error(`Expected only one separator in localization expression. Got ${pair.length - 1}`);
+   }
+   return {
+      text: (pair.pop() || EMPTY_STRING).trim(),
+      context: (pair.pop() || EMPTY_STRING).trim()
+   };
+}
+
+export interface IDictionaryItem {
+   module: string;
+   text: string;
+   context: string;
+}
+
+export class Dictionary {
+   private readonly items: IDictionaryItem[];
+
+   constructor() {
+      this.items = [];
+   }
+
+   push(module: string, text: string, context: string = EMPTY_STRING): void {
+      if (text.trim().length === 0) {
+         return;
+      }
+      this.items.push({
+         module,
+         text,
+         context
+      });
+   }
+
+   getItems(): IDictionaryItem[] {
+      return this.items;
+   }
+}
