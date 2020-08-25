a5582b5fc78a9ac2432d3e2099399be20f500caf
ma.krylov <ma.krylov@tensor.ru>
2020-08-25 05:39:49 +0300

    patch
    https://online.sbis.ru/opendoc.html?guid=e238a42e-fedf-4601-bb71-e77dd475cff6

--- UI/_builder/Tmpl/core/PatchVisitor.ts
+++ UI/_builder/Tmpl/core/PatchVisitor.ts
@@ -10,113 +10,170 @@
 
 import * as Ast from './Ast';
 
+interface INavigationContext {
+   parent: Ast.Ast | null;
+}
+
+function getTagType(name: string): string {
+   if (name === 'script') {
+      return name;
+   }
+   if (name === 'style') {
+      return name;
+   }
+   return 'tag';
+}
+
 class PatchVisitor implements Ast.IAstVisitor {
-   visitAttribute(node: Ast.AttributeNode, context: any): any {
+   visitAttribute(node: Ast.AttributeNode, context: INavigationContext): any {
       return node;
    }
 
-   visitOption(node: Ast.OptionNode, context: any): any {
+   visitOption(node: Ast.OptionNode, context: INavigationContext): any {
       return node;
    }
 
-   visitBind(node: Ast.BindNode, context: any): any {
+   visitBind(node: Ast.BindNode, context: INavigationContext): any {
       return node;
    }
 
-   visitEvent(node: Ast.EventNode, context: any): any {
+   visitEvent(node: Ast.EventNode, context: INavigationContext): any {
       return node;
    }
 
-   visitElement(node: Ast.ElementNode, context: any): any {
+   visitElement(node: Ast.ElementNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = getTagType(node.name);
+   }
+
+   visitDoctype(node: Ast.DoctypeNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'directive';
+      // @ts-ignore
+      node.name = '!DOCTYPE';
+      // @ts-ignore
+      node.data = `!DOCTYPE ${node.data}`;
+   }
+
+   visitCData(node: Ast.CDataNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'directive';
+      // @ts-ignore
+      node.name = '![CDATA[';
+      // @ts-ignore
+      node.data = `![CDATA[${node.data}]]`;
+   }
+
+   visitInstruction(node: Ast.InstructionNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'directive';
+      // @ts-ignore
+      node.name = '?';
+   }
+
+   visitComment(node: Ast.CommentNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'comment';
+   }
+
+   visitComponent(node: Ast.ComponentNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitPartial(node: Ast.PartialNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitTemplate(node: Ast.TemplateNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitIf(node: Ast.IfNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitElse(node: Ast.ElseNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitFor(node: Ast.ForNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitForeach(node: Ast.ForeachNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitArray(node: Ast.ArrayNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitBoolean(node: Ast.BooleanNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitFunction(node: Ast.FunctionNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitNumber(node: Ast.NumberNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitObject(node: Ast.ObjectNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitString(node: Ast.StringNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'tag';
+   }
+
+   visitValue(node: Ast.ValueNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = getTagType(node.name);
+   }
+
+   visitText(node: Ast.TextNode, context: INavigationContext): any {
+      // @ts-ignore
+      node.type = 'text';
+   }
+
+   visitTextData(node: Ast.TextDataNode, context: INavigationContext): any {
       return node;
    }
 
-   visitDoctype(node: Ast.DoctypeNode, context: any): any {
+   visitExpression(node: Ast.ExpressionNode, context: INavigationContext): any {
       return node;
    }
 
-   visitCData(node: Ast.CDataNode, context: any): any {
+   visitTranslation(node: Ast.TranslationNode, context: INavigationContext): any {
       return node;
    }
 
-   visitInstruction(node: Ast.InstructionNode, context: any): any {
-      return node;
-   }
-
-   visitComment(node: Ast.CommentNode, context: any): any {
-      return node;
-   }
-
-   visitComponent(node: Ast.ComponentNode, context: any): any {
-      return node;
-   }
-
-   visitPartial(node: Ast.PartialNode, context: any): any {
-      return node;
-   }
-
-   visitTemplate(node: Ast.TemplateNode, context: any): any {
-      return node;
-   }
-
-   visitIf(node: Ast.IfNode, context: any): any {
-      return node;
-   }
-
-   visitElse(node: Ast.ElseNode, context: any): any {
-      return node;
-   }
-
-   visitFor(node: Ast.ForNode, context: any): any {
-      return node;
-   }
-
-   visitForeach(node: Ast.ForeachNode, context: any): any {
-      return node;
-   }
-
-   visitArray(node: Ast.ArrayNode, context: any): any {
-      return node;
-   }
-
-   visitBoolean(node: Ast.BooleanNode, context: any): any {
-      return node;
-   }
-
-   visitFunction(node: Ast.FunctionNode, context: any): any {
-      return node;
-   }
-
-   visitNumber(node: Ast.NumberNode, context: any): any {
-      return node;
-   }
-
-   visitObject(node: Ast.ObjectNode, context: any): any {
-      return node;
-   }
-
-   visitString(node: Ast.StringNode, context: any): any {
-      return node;
-   }
-
-   visitValue(node: Ast.ValueNode, context: any): any {
-      return node;
-   }
-
-   visitText(node: Ast.TextNode, context: any): any {
-      return node;
-   }
-
-   visitTextData(node: Ast.TextDataNode, context: any): any {
-      return node;
-   }
-
-   visitExpression(node: Ast.ExpressionNode, context: any): any {
-      return node;
-   }
-
-   visitTranslation(node: Ast.TranslationNode, context: any): any {
-      return node;
+   visitAll(nodes: Ast.Ast[], context?: INavigationContext): any {
+      for (let i = 0; i < nodes.length; ++i) {
+         nodes[i].accept(this, context);
+         // @ts-ignore
+         nodes[i].prev = nodes[i - 1] || null;
+         // @ts-ignore
+         nodes[i].next = nodes[i + 1] || null;
+         // @ts-ignore
+         nodes[i].parent = context && context.parent || null;
+      }
+      return nodes;
    }
 }
 
