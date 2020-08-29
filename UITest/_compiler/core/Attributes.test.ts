cfc267954a7dfafc304076308d5bb769685ce028
ma.krylov <ma.krylov@tensor.ru>
2020-08-29 20:32:57 +0300

    add attribute processor tests
    https://online.sbis.ru/opendoc.html?guid=e238a42e-fedf-4601-bb71-e77dd475cff6

--- UITest/_compiler/core/Attributes.test.ts
+++ UITest/_compiler/core/Attributes.test.ts
@@ -45,16 +45,37 @@ function createAttributeProcessorOptions(hasAttributesOnly: boolean) {
    };
 }
 
-function processAttributes(attributes: string, hasAttributesOnly: boolean) {
+function parseAttributes(attributes: string, hasAttributesOnly: boolean) {
    const text = `<div ${attributes}></div>`;
-   const config = createAttributeProcessorConfig();
-   const options = createAttributeProcessorOptions(hasAttributesOnly);
-   const processor = Attributes.createAttributeProcessor(config);
    const html = parse(text, FILE_NAME, parseConfig);
    assert.strictEqual(html.length, 1);
    assert.isTrue(html[0] instanceof Nodes.Tag);
    const tag = <Nodes.Tag>html[0];
-   return processor.process(tag.attributes, options);
+   return tag.attributes;
+}
+
+function processAttributes(textAttributes: string, hasAttributesOnly: boolean) {
+   const attributes = parseAttributes(textAttributes, hasAttributesOnly);
+   const options = createAttributeProcessorOptions(hasAttributesOnly);
+   const config = createAttributeProcessorConfig();
+   const processor = Attributes.createAttributeProcessor(config);
+   return processor.process(attributes, options);
+}
+
+function filterAttributes(textAttributes: string, expected: string[], hasAttributesOnly: boolean) {
+   const attributes = parseAttributes(textAttributes, hasAttributesOnly);
+   const options = createAttributeProcessorOptions(hasAttributesOnly);
+   const config = createAttributeProcessorConfig();
+   const processor = Attributes.createAttributeProcessor(config);
+   return processor.filter(attributes, expected, options);
+}
+
+function validateValueAttributes(textAttributes: string, name: string, hasAttributesOnly: boolean) {
+   const attributes = parseAttributes(textAttributes, hasAttributesOnly);
+   const options = createAttributeProcessorOptions(hasAttributesOnly);
+   const config = createAttributeProcessorConfig();
+   const processor = Attributes.createAttributeProcessor(config);
+   return processor.validateValue(attributes, name, options);
 }
 
 describe('Compiler/core/Attributes', () => {
@@ -98,8 +119,83 @@ describe('Compiler/core/Attributes', () => {
          assert.strictEqual(Attributes.getEventName('click'), 'click');
       });
    });
-   it('test', () => {
-      // TODO: In development...
-      processAttributes('attr=value', true);
+   describe('process @ AttributeProcessor', () => {
+      describe('.hasAttributesOnly = true', () => {
+         it('Attribute (without prefix)', () => {
+            const attributes = processAttributes('attribute="value"', true);
+            assert.strictEqual(Object.keys(attributes.attributes).length, 1);
+            assert.strictEqual(Object.keys(attributes.options).length, 0);
+            assert.strictEqual(Object.keys(attributes.events).length, 0);
+            assert.isTrue(attributes.attributes.hasOwnProperty('attr:attribute'));
+         });
+         it('Attribute (with prefix)', () => {
+            const attributes = processAttributes('attr:attribute="value"', true);
+            assert.strictEqual(Object.keys(attributes.attributes).length, 1);
+            assert.strictEqual(Object.keys(attributes.options).length, 0);
+            assert.strictEqual(Object.keys(attributes.events).length, 0);
+            assert.isTrue(attributes.attributes.hasOwnProperty('attr:attribute'));
+         });
+         it('Bind', () => {
+            const attributes = processAttributes('bind:attribute="value"', true);
+            assert.strictEqual(Object.keys(attributes.attributes).length, 0);
+            assert.strictEqual(Object.keys(attributes.options).length, 0);
+            assert.strictEqual(Object.keys(attributes.events).length, 1);
+            assert.isTrue(attributes.events.hasOwnProperty('bind:attribute'));
+         });
+         it('Event handler', () => {
+            const attributes = processAttributes('on:event="handler()"', true);
+            assert.strictEqual(Object.keys(attributes.attributes).length, 0);
+            assert.strictEqual(Object.keys(attributes.options).length, 0);
+            assert.strictEqual(Object.keys(attributes.events).length, 1);
+            assert.isTrue(attributes.events.hasOwnProperty('on:event'));
+         });
+      });
+      describe('.hasAttributesOnly = false', () => {
+         it('Option', () => {
+            const attributes = processAttributes('attribute="value"', false);
+            assert.strictEqual(Object.keys(attributes.attributes).length, 0);
+            assert.strictEqual(Object.keys(attributes.options).length, 1);
+            assert.strictEqual(Object.keys(attributes.events).length, 0);
+            assert.isTrue(attributes.options.hasOwnProperty('attribute'));
+         });
+         it('Attribute', () => {
+            const attributes = processAttributes('attr:attribute="value"', false);
+            assert.strictEqual(Object.keys(attributes.attributes).length, 1);
+            assert.strictEqual(Object.keys(attributes.options).length, 0);
+            assert.strictEqual(Object.keys(attributes.events).length, 0);
+            assert.isTrue(attributes.attributes.hasOwnProperty('attr:attribute'));
+         });
+      });
+   });
+   describe('filter @ AttributeProcessor', () => {
+      it('Has the same start', () => {
+         const attributes = filterAttributes('attribute="value"', ['attributeKey'], false);
+         assert.strictEqual(Object.keys(attributes).length, 0);
+      });
+      it('Filter', () => {
+         const attributes = filterAttributes('a=1 b=2 c=3 d=4 elephant=5', ['a', 'c', 'f', 'apple', 'e'], false);
+         assert.strictEqual(Object.keys(attributes).length, 2);
+         assert.isTrue(attributes.hasOwnProperty('a'));
+         assert.isTrue(attributes.hasOwnProperty('c'));
+      });
+   });
+   describe('validateValue @ AttributeProcessor', () => {
+      it('Receive value', (done) => {
+         try {
+            const value = validateValueAttributes('attribute="value"', 'attribute', false);
+            assert.strictEqual(value, 'value');
+            done();
+         } catch (error) {
+            done(error);
+         }
+      });
+      it('No attribute', (done) => {
+         try {
+            validateValueAttributes('attribute="value"', 'key', false);
+            done(new Error('Must be failed'));
+         } catch (error) {
+            done();
+         }
+      });
    });
 });
