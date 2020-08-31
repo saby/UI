a21bdb30db5a40a5b8d5a927bdb154022e4b969a
ma.krylov <ma.krylov@tensor.ru>
2020-08-31 05:50:40 +0300

    upd methods
    https://online.sbis.ru/doc/e238a42e-fedf-4601-bb71-e77dd475cff6

--- UITest/_compiler/NullLogger.ts
+++ UITest/_compiler/NullLogger.ts
@@ -3,12 +3,11 @@
 import ErrorHandler, { IErrorHandler, ILogger } from 'UI/_builder/Tmpl/utils/ErrorHandler';
 
 class NullLogger implements ILogger {
-   log(message: string): void { }
-   warn(message: string): void { }
-   error(message: string): void { }
+   log(): void { }
+   warn(): void { }
+   error(): void { }
 }
 
 export default function createErrorHandler(): IErrorHandler {
    return new ErrorHandler(new NullLogger());
 }
-
