b1151155144604b80a45f8cf5577e3e0a9204e4e
va.ten <va.ten@tensor.ru>
2020-09-01 17:38:17 +0300

    https://online.sbis.ru/opendoc.html?guid=7c9c6cf6-8a70-4bb0-872d-fc9f01c95b22

---
+++ UIDemo/MobileEventDemo/LongTap.ts
@@ -0,0 +1,35 @@
+/// <amd-module name="UIDemo/MobileEventDemo/LongTap" />
+
+import {Control} from 'UI/Base';
+
+// @ts-ignore
+import template = require('wml!UIDemo/MobileEventDemo/LongTap');
+
+class LongTap extends Control {
+   _template = template;
+
+   private longTapCount: number = null;
+   private swipeCount: number = null;
+   private tapCount: number = null;
+
+   _beforeMount() {
+      this.longTapCount = 0;
+      this.swipeCount = 0;
+      this.tapCount = 0;
+   }
+
+   private longClick() {
+      this.longTapCount += 1;
+   }
+
+   private simpleClick() {
+      this.tapCount += 1;
+   }
+   private swipe() {
+      this.swipeCount += 1;
+   }
+}
+
+LongTap._styles = ['UIDemo/MobileEventDemo/MobileEventDemo'];
+
+export = LongTap;
---
+++ UIDemo/MobileEventDemo/Swipe.ts
@@ -0,0 +1,30 @@
+/// <amd-module name="UIDemo/MobileEventDemo/Swipe" />
+
+import {Control} from 'UI/Base';
+
+// @ts-ignore
+import template = require('wml!UIDemo/MobileEventDemo/Swipe');
+
+class Swipe extends Control {
+   _template = template;
+
+   private swipeCount: number = null;
+   private tapCount: number = null;
+
+   _beforeMount() {
+      this.swipeCount = 0;
+      this.tapCount = 0;
+   }
+
+   private simpleClick() {
+      this.tapCount += 1;
+   }
+
+   private swipe() {
+      this.swipeCount += 1;
+   }
+}
+
+Swipe._styles = ['UIDemo/MobileEventDemo/MobileEventDemo'];
+
+export = Swipe;
