36ba471f512c1e0ed342378671a6b36a442381e5
ma.krylov <ma.krylov@tensor.ru>
2020-09-03 01:46:02 +0300

    upd null logger
    https://online.sbis.ru/doc/e238a42e-fedf-4601-bb71-e77dd475cff6

--- UITest/_compiler/NullLogger.ts
+++ UITest/_compiler/NullLogger.ts
@@ -1,13 +1,23 @@
 /// <amd-module name="UITest/_compiler/NullLogger" />
 
-import ErrorHandler, { IErrorHandler, ILogger } from 'UI/_builder/Tmpl/utils/ErrorHandler';
+import ErrorHandler, { IErrorHandler, ILogger, Logger } from 'UI/_builder/Tmpl/utils/ErrorHandler';
 
+/**
+ * Null logger to keep console in browser clean.
+ */
 class NullLogger implements ILogger {
    log(): void { }
    warn(): void { }
    error(): void { }
 }
 
-export default function createErrorHandler(): IErrorHandler {
-   return new ErrorHandler(new NullLogger());
+/**
+ * Create error handler with or without null logger.
+ * @param useNullLogger {boolean} Use null logger. True by default.
+ */
+export default function createErrorHandler(useNullLogger: boolean = true): IErrorHandler {
+   if (useNullLogger) {
+      return new ErrorHandler(new NullLogger());
+   }
+   return new ErrorHandler(new Logger());
 }
