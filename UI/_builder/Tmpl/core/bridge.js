a6cdb54cc72e591891603f215eb105d3d5d219e8
ma.krylov <ma.krylov@tensor.ru>
2020-09-04 19:45:14 +0300

    upd bridge
    https://online.sbis.ru/doc/e238a42e-fedf-4601-bb71-e77dd475cff6

--- UI/_builder/Tmpl/core/bridge.js
+++ UI/_builder/Tmpl/core/bridge.js
@@ -6,8 +6,21 @@ define('UI/_builder/Tmpl/core/bridge', [
    'UI/_builder/Tmpl/utils/ErrorHandler',
    'UI/_builder/Tmpl/core/PatchVisitor',
    'Core/Deferred',
-   'UI/_builder/Tmpl/core/Scope'
-], function(traversing, TraverseLib, postTraverse, ParserLib, ErrorHandlerLib, PatchVisitorLib, Deferred, ScopeLib) {
+   'UI/_builder/Tmpl/core/Scope',
+   'UI/_builder/Tmpl/expressions/_private/DirtyCheckingPatch',
+   'Core/helpers/Array/uniq'
+], function(
+   traversing,
+   TraverseLib,
+   postTraverse,
+   ParserLib,
+   ErrorHandlerLib,
+   PatchVisitorLib,
+   Deferred,
+   ScopeLib,
+   dirtyCheckingPatch,
+   uniqueArray
+) {
    'use strict';
    var USE_VISITOR = false;
 
@@ -25,11 +38,41 @@ define('UI/_builder/Tmpl/core/bridge', [
       };
       var traversed = TraverseLib.default(htmlTree, traverseConfig, traverseOptions);
       PatchVisitorLib.default(traversed, traverseOptions.scope);
-      postTraverse.call({
-         createResultDictionary: options.createResultDictionary,
-         words: options.words,
-         fileName: options.fileName
-      }, deferred, traversed);
+
+      // post traverse
+
+      traversed.__newVersion = true;
+      var foundVars = [];
+      var foundChildren = [];
+      for (var travI = 0; travI < traversed.length; travI++) {
+         try {
+            foundVars = foundVars.concat(dirtyCheckingPatch.gatherReactive(traversed[travI]));
+            foundChildren = foundChildren.concat(traversed[travI].childrenStorage || []);
+         } catch (error) {
+            deferred.errback(new Error(
+               'Something wrong with ' + this.fileName + ' template. ' + error.message
+            ));
+            return undefined;
+         }
+      }
+
+      // формируем набор реактивных свойств, "служебные" свойства игнорируем
+      traversed.reactiveProps = uniqueArray(foundVars).filter(function(item) {
+         return item !== '...' && item !== '_options' && item !== '_container' && item !== '_children';
+      });
+
+      traversed.childrenStorage = foundChildren;
+
+      // в случае сбора словаря локализуемых слов отдаем объект
+      // { astResult - ast-дерево, words - словарь локализуемых слов }
+      if (this.createResultDictionary) {
+         deferred.callback({
+            astResult: traversed,
+            words: this.words
+         });
+      } else {
+         deferred.callback(traversed);
+      }
       return deferred;
    }
 
