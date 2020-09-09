a2c4c803398eb3989abcb62dcb38d764c73feff5
ma.krylov <ma.krylov@tensor.ru>
2020-09-09 08:35:15 +0300

    update bridge
    https://online.sbis.ru/doc/e238a42e-fedf-4601-bb71-e77dd475cff6

--- UI/_builder/Tmpl/core/bridge.js
+++ UI/_builder/Tmpl/core/bridge.js
@@ -7,7 +7,9 @@ define('UI/_builder/Tmpl/core/bridge', [
    'UI/_builder/Tmpl/core/PatchVisitor',
    'Core/Deferred',
    'UI/_builder/Tmpl/core/Scope',
-   'UI/_builder/Tmpl/i18n/Translator'
+   'UI/_builder/Tmpl/i18n/Translator',
+   'UI/_builder/Tmpl/expressions/_private/DirtyCheckingPatch',
+   'UI/Utils'
 ], function(
    traversing,
    TraverseLib,
@@ -17,10 +19,14 @@ define('UI/_builder/Tmpl/core/bridge', [
    PatchVisitorLib,
    Deferred,
    ScopeLib,
-   Translator
+   Translator,
+   dirtyCheckingPatch,
+   Utils
 ) {
    'use strict';
    var USE_VISITOR = false;
+
+   var uniqueArray = Utils.ArrayUtils.uniq;
 
    function traverseWithVisitors(htmlTree, options) {
       var deferred = new Deferred();
@@ -49,12 +55,38 @@ define('UI/_builder/Tmpl/core/bridge', [
          return deferred;
       }
       scope.requestDependencies().then(function() {
-         // post traverse
-         postTraverse.call({
-            createResultDictionary: options.createResultDictionary,
-            words: scope.getTranslationKeys(),
-            fileName: options.fileName
-         }, deferred, traversed);
+         traversed.__newVersion = true;
+         var foundVars = [];
+         var foundChildren = [];
+         for (var index = 0; index < traversed.length; index++) {
+            try {
+               foundVars = foundVars.concat(dirtyCheckingPatch.gatherReactive(traversed[index]));
+               foundChildren = foundChildren.concat(traversed[index].childrenStorage || []);
+            } catch (error) {
+               deferred.errback(new Error(
+                  'При выполнении аннотирования AST-дерева шаблона "' + options.fileName + '" возникла ошибка: ' + error.message
+               ));
+               return;
+            }
+         }
+
+         // формируем набор реактивных свойств, "служебные" свойства игнорируем
+         traversed.reactiveProps = uniqueArray(foundVars).filter(function(item) {
+            return item !== '...' && item !== '_options' && item !== '_container' && item !== '_children';
+         });
+
+         traversed.childrenStorage = foundChildren;
+
+         // в случае сбора словаря локализуемых слов отдаем объект
+         // { astResult - ast-дерево, words - словарь локализуемых слов }
+         if (options.createResultDictionary) {
+            deferred.callback({
+               astResult: traversed,
+               words: scope.getTranslationKeys()
+            });
+            return;
+         }
+         deferred.callback(traversed);
       });
       return deferred;
    }
