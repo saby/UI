99508d70acb5a836ff55d78e0003ce4d288b4466
ma.krylov <ma.krylov@tensor.ru>
2020-10-14 01:24:13 +0300

    set return value type
    https://online.sbis.ru/opendoc.html?guid=b36b9efb-1d27-4157-b3b0-0a90b25f6458

--- UI/_builder/Tmpl/core/Annotate.ts
+++ UI/_builder/Tmpl/core/Annotate.ts
@@ -17,9 +17,16 @@ export interface IAnnotateProcessor {
    annotate(nodes: Ast.Ast[]): IAnnotatedTree;
 }
 
-interface IContext {
-   // TODO: Release
+interface IStorage {
+   [name: string]: boolean;
 }
+
+interface IContext {
+   childrenStorage: string[];
+   identifiersStore: IStorage;
+}
+
+const EMPTY_ARRAY = [];
 
 class AnnotateProcessor implements Ast.IAstVisitor, IAnnotateProcessor {
 
@@ -30,110 +37,148 @@ class AnnotateProcessor implements Ast.IAstVisitor, IAnnotateProcessor {
 
    // <editor-fold desc="Data types">
 
-   visitArray(node: Ast.ArrayNode, context: IContext): any {
-      node.__$ws_elements.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitArray(node: Ast.ArrayNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_elements.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
+      return expressions;
    }
 
-   visitBoolean(node: Ast.BooleanNode, context: IContext): any {
-      node.__$ws_data.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitBoolean(node: Ast.BooleanNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_data.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
+      return expressions;
    }
 
-   visitFunction(node: Ast.FunctionNode, context: IContext): any {
-      node.__$ws_functionExpression.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitFunction(node: Ast.FunctionNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_functionExpression.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
       for (const name in node.__$ws_options) {
          const property = node.__$ws_options[name];
-         property.accept(this, context);
+         expressions = expressions.concat(property.accept(this, context));
       }
+      return expressions;
    }
 
-   visitNumber(node: Ast.NumberNode, context: IContext): any {
-      node.__$ws_data.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitNumber(node: Ast.NumberNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_data.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
+      return expressions;
    }
 
-   visitObject(node: Ast.ObjectNode, context: IContext): any {
+   visitObject(node: Ast.ObjectNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
       for (const name in node.__$ws_properties) {
          const property = node.__$ws_properties[name];
-         property.accept(this, context);
+         expressions = expressions.concat(property.accept(this, context));
       }
+      return expressions;
    }
 
-   visitString(node: Ast.StringNode, context: IContext): any {
-      node.__$ws_data.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitString(node: Ast.StringNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_data.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
+      return expressions;
    }
 
-   visitValue(node: Ast.ValueNode, context: IContext): any {
-      node.__$ws_data.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitValue(node: Ast.ValueNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_data.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
+      return expressions;
    }
 
    // </editor-fold>
 
    // <editor-fold desc="Attributes and options">
 
-   visitAttribute(node: Ast.AttributeNode, context: IContext): any {
-      node.__$ws_value.forEach((node: Ast.Ast) => node.accept(this, context));
+   visitAttribute(node: Ast.AttributeNode, context: IContext): Ast.ExpressionNode[] {
+      let expressions = [];
+      node.__$ws_value.forEach((node: Ast.Ast) => {
+         expressions = expressions.concat(node.accept(this, context));
+      });
+      return expressions;
    }
 
-   visitBind(node: Ast.BindNode, context: IContext): any {
+   visitBind(node: Ast.BindNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitEvent(node: Ast.EventNode, context: IContext): any {
+   visitEvent(node: Ast.EventNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitOption(node: Ast.OptionNode, context: IContext): any {
-      node.__$ws_value.accept(this, context);
+   visitOption(node: Ast.OptionNode, context: IContext): Ast.ExpressionNode[] {
+      return node.__$ws_value.accept(this, context);
    }
 
    // </editor-fold>
 
    // <editor-fold desc="HTML nodes">
 
-   visitCData(node: Ast.CDataNode, context: IContext): any { }
+   visitCData(node: Ast.CDataNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
-   visitComment(node: Ast.CommentNode, context: IContext): any { }
+   visitComment(node: Ast.CommentNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
-   visitContentOption(node: Ast.ContentOptionNode, context: IContext): any {
+   visitContentOption(node: Ast.ContentOptionNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitDoctype(node: Ast.DoctypeNode, context: IContext): any { }
+   visitDoctype(node: Ast.DoctypeNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
-   visitElement(node: Ast.ElementNode, context: IContext): any {
+   visitElement(node: Ast.ElementNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitInstruction(node: Ast.InstructionNode, context: IContext): any { }
+   visitInstruction(node: Ast.InstructionNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
    // </editor-fold>
 
    // <editor-fold desc="Directives">
 
-   visitElse(node: Ast.ElseNode, context: IContext): any {
+   visitElse(node: Ast.ElseNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitFor(node: Ast.ForNode, context: IContext): any {
+   visitFor(node: Ast.ForNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitForeach(node: Ast.ForeachNode, context: IContext): any {
+   visitForeach(node: Ast.ForeachNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitIf(node: Ast.IfNode, context: IContext): any {
+   visitIf(node: Ast.IfNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitTemplate(node: Ast.TemplateNode, context: IContext): any {
+   visitTemplate(node: Ast.TemplateNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
@@ -142,43 +187,48 @@ class AnnotateProcessor implements Ast.IAstVisitor, IAnnotateProcessor {
 
    // <editor-fold desc="Extended text">
 
-   visitExpression(node: Ast.ExpressionNode, context: IContext): any {
+   visitExpression(node: Ast.ExpressionNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitText(node: Ast.TextNode, context: IContext): any { }
+   visitText(node: Ast.TextNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
-   visitTextData(node: Ast.TextDataNode, context: IContext): any { }
+   visitTextData(node: Ast.TextDataNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
-   visitTranslation(node: Ast.TranslationNode, context: IContext): any { }
+   visitTranslation(node: Ast.TranslationNode, context: IContext): Ast.ExpressionNode[] {
+      return EMPTY_ARRAY;
+   }
 
    // </editor-fold>
 
    // <editor-fold desc="Components and templates">
 
-   visitComponent(node: Ast.ComponentNode, context: IContext): any {
+   visitComponent(node: Ast.ComponentNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitDynamicPartial(node: Ast.DynamicPartialNode, context: IContext): any {
+   visitDynamicPartial(node: Ast.DynamicPartialNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitInlineTemplate(node: Ast.InlineTemplateNode, context: IContext): any {
+   visitInlineTemplate(node: Ast.InlineTemplateNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
-   visitStaticPartial(node: Ast.StaticPartialNode, context: IContext): any {
+   visitStaticPartial(node: Ast.StaticPartialNode, context: IContext): Ast.ExpressionNode[] {
       // TODO: Release
       throw new Error('Not implemented yet');
    }
 
    // </editor-fold>
-
 }
 
 export default function annotate(nodes: Ast.Ast[]): IAnnotatedTree {
