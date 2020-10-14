331d2e7761c6e4786d0cc41258907f659f9ac3b8
va.ten <va.ten@tensor.ru>
2020-10-14 23:08:12 +0300

    https://online.sbis.ru/opendoc.html?guid=26e630ff-645f-4096-9ea6-ef51e69a4e0c

--- UI/_executor/_Markup/Generator.ts
+++ UI/_executor/_Markup/Generator.ts
@@ -526,7 +526,14 @@ export class Generator {
       controlClass = resolverTpl.controlClass;
       dataComponent = resolverTpl.dataComponent;
 
+      const fromOld = controlClass && controlClass.prototype && Common.isCompound(controlClass);
+
       const controlProperties = Scope.calculateScope(scope, Common.plainMerge) || {};
+      if (fromOld) {
+         for (let key in attrs.events) {
+            controlProperties[key] = attrs.events[key];
+         }
+      }
 
       if (!attrs.attributes) {
          attrs.attributes = {};
@@ -544,7 +551,27 @@ export class Generator {
       OptionsResolver.resolveInheritOptions(controlClass, attrs, controlProperties);
 
       if (Common.isCompat()) {
-         isCompatPatch(controlClass, controlProperties, attrs);
+         if (controlProperties && controlProperties.enabled === undefined) {
+            const internal = attrs.internal;
+            if (internal && internal.parent && fromOld) {
+               if (internal.parentEnabled !== undefined && controlProperties.allowChangeEnable !== false) {
+                  controlProperties.enabled = internal.parentEnabled;
+               } else {
+                  controlProperties.enabled = true;
+               }
+            } else if (fromOld && internal.parentEnabled === false) {
+               controlProperties.__enabledOnlyToTpl = internal.parentEnabled;
+            }
+         }
+
+         if (fromOld) {
+            const objForFor = attrs.attributes;
+            for (let i in objForFor) {
+               if (objForFor.hasOwnProperty(i) && EventUtils.isEvent(i)) {
+                  controlProperties[i] = objForFor[i];
+               }
+            }
+         }
       }
 
       return {
