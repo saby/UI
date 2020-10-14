cb457798edbc2724359f8cc035d585835305595d
ma.krylov <ma.krylov@tensor.ru>
2020-10-14 15:54:57 +0300

    [diff] add legacy error
    https://online.sbis.ru/opendoc.html?guid=b36b9efb-1d27-4157-b3b0-0a90b25f6458

--- UI/_builder/Tmpl/core/Annotate.ts
+++ UI/_builder/Tmpl/core/Annotate.ts
@@ -470,7 +470,10 @@ class AnnotateProcessor implements Ast.IAstVisitor, IAnnotateProcessor {
    visitContentOption(node: Ast.ContentOptionNode, context: IContext): Ast.ExpressionNode[] {
       const ignoredIdentifiers: IStorage = { };
       let expressions: Ast.ExpressionNode[] = [];
-      ignoredIdentifiers[node.__$ws_name] = true;
+      // FIXME: Legacy error
+      if (!node.hasFlag(Ast.Flags.NEST_CASTED)) {
+         ignoredIdentifiers[node.__$ws_name] = true;
+      }
       node.__$ws_content.forEach((node: Ast.Ast) => {
          expressions = expressions.concat(node.accept(this, context));
       });
