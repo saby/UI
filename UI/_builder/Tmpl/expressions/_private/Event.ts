3eb2fecf22e65ea4cbdb2aef54e39836f7620754
aa.shipin <aa.shipin@tensor.ru>
2020-10-20 15:24:24 +0300

    https://online.sbis.ru/opendoc.html?guid=1bcb5f28-56ce-4bb0-a667-3205dac35f1b

--- UI/_builder/Tmpl/expressions/_private/Event.ts
+++ UI/_builder/Tmpl/expressions/_private/Event.ts
@@ -4,98 +4,17 @@
  * @author Крылов М.А.
  */
 
-import { VariableNode } from './Statement';
 import { EventExpressionVisitor } from './Nodes';
+import {EventNode} from "./EventNode";
+import {EventChain} from "./EventChain";
+import {IAttributeValue} from "./IAttributeValue";
 
 import * as FSC from 'UI/_builder/Tmpl/modules/data/utils/functionStringCreator';
-import * as templates from 'UI/_builder/Tmpl/codegen/templates';
 
 /**
  * Паттерн обработчика события для имени атрибута.
  */
 const EVENT_NAME_PATTERN = /^(on:[A-z0-9])\w*$/;
-
-/**
- * Класс цепочки обработчиков. Содержит коллекцию узлов EventNode для конкретного события.
- *
- * ВАЖНО: в начале идут bind-обработчики, а затем - события.
- */
-export class EventChain extends Array<EventNode> {
-   /**
-    * Флаг, по которому различается EventChain от обычного массива.
-    * @todo Избыточный флаг. Отрефакторить код и избавиться от него.
-    */
-   readonly events: boolean = true;
-}
-
-/**
- * Подготовить цепочку обработчиков: создать массив или вернуть имеющийся.
- * @param originChain {EventChain} Исходная цепочка обработчиков для конкретного события либо undefined.
- */
-export function prepareEventChain(originChain?: EventChain): EventChain {
-   if (!originChain) {
-      return new EventChain();
-   }
-   return originChain;
-}
-
-/**
- * Данный класс представляет узел обработчика события.
- */
-export class EventNode {
-   /**
-    * Имя узла - 'event'.
-    * @todo Избыточный флаг. Отрефакторить код и избавиться от него.
-    */
-   name: string;
-   /**
-    * Аргументы, переданные функции-обработчику события.
-    */
-   args: string;
-   /**
-    * Имя функции-обработчика события.
-    */
-   value: string;
-   /**
-    * Сгенерированный код для обработчика события.
-    */
-   fn: string;
-
-   /**
-    * Инициализировать новый узел.
-    * @param name {string} Имя события.
-    * @param args {string} Аргументы обработчика.
-    * @param value {string} Имя функции-обработчика.
-    * @param fn {string} Сгенерированная функция-обработчик.
-    */
-   constructor(name: string, args: string, value: string, fn: string) {
-      this.name = name;
-      this.args = args;
-      this.value = value;
-      this.fn = fn;
-   }
-}
-
-/**
- * Интерфейст значения атрибута.
- * @todo Отрефакторить парсеры и перенести интерфейс в определения Wasaby-узлов.
- */
-export interface IAttributeValue {
-   /**
-    * Значение атрибута можеть быть "склейкой" разных узлов - локализация, текст, выражение.
-    */
-   data: VariableNode[];
-   /**
-    * Флаг, необходимый для различия опций контрола от атрибутов.
-    * @todo Избыточный флаг. Отрефакторить код и избавиться от него.
-    */
-   property: boolean;
-   /**
-    * Тип атрибута.
-    * @todo Избыточный флаг. Отрефакторить код и избавиться от него.
-    */
-   type: string;
-}
 
 /**
  * Создать узел обработчика события по атрибуту.
@@ -131,16 +50,17 @@ export function processEventAttribute(
       checkChildren: true
    };
    const artifact = eventVisitor.visit(value.data[0].name, eventContext);
-   const handler = FSC.wrapAroundExec(
-      templates.generateEvent(
-         artifact.fn,
-         artifact.context,
-         isControl
-      )
-   );
+   const handler = FSC.wrapAroundExec('function() { return ' + artifact.fn + '; }');
    const eventArguments = FSC.wrapAroundExec(`[${artifact.args.join(',')}]`);
-   const chain = prepareEventChain();
-   chain.push(new EventNode('event', eventArguments, artifact.handlerName, handler));
+   const chain = EventChain.prepareEventChain();
+   chain.push(new EventNode({
+         args: eventArguments,
+         value: artifact.handlerName,
+         viewController: FSC.wrapAroundExec('viewController'),
+         handler: handler,
+         isControl: isControl,
+         context: FSC.wrapAroundExec('(function(){ return ' + artifact.context + '; })')
+      }));
    return chain;
 }
 
