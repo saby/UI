fb19b0c4a6af6cf82d5183692ee15eed8984cd90
Кудрявцев И.С <is.kudryavcev@corp.tensor.ru>
2020-11-02 13:52:26 +0300

    https://online.sbis.ru/opendoc.html?guid=edd0be98-1f96-4288-8db9-e8b90bdd5852

--- UI/_base/DepsCollector.ts
+++ UI/_base/DepsCollector.ts
@@ -447,6 +447,12 @@ export class DepsCollector {
 
          const loadedResources = loadedContexts[UIModuleName];
 
+         if (controller.loadingsHistory.contexts.hasOwnProperty(UIModuleName)) {
+
+            // Добавляем contents для модулей с внешних сервисов, в них инофрмация о доступных ресурсах локализации.
+            files.js.push(controller.loadingsHistory.contexts[UIModuleName]);
+         }
+
          if (loadedResources.hasOwnProperty(localeCode)) {
             this.addLocalizationResource(files, loadedResources[localeCode]);
 
