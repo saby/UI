c70e163f3fc9d70ad2c94ea8753e8c0399793f74
li.mustafin <>
2020-11-16 14:09:14 +0300

    https://online.sbis.ru/opendoc.html?guid=65fd121f-cb26-45ae-bf44-d77bf93e0c48
    Revert "https://online.sbis.ru/opendoc.html?guid=bbf39ec3-b791-4763-911e-8bfbe943ed1a"
    This reverts commit 493719d9ccb85abd47810902a3bc1f7ce7db217b.

--- UI/_base/PageDeps.ts
+++ UI/_base/PageDeps.ts
@@ -81,10 +81,6 @@ function getModulesDeps(modules: IModules = {}): IModulesDescription {
    const externalPaths = Object.keys(modules)
       .filter((name) => !!modules[name].path)
       .map((name) => {
-         // пути внешних сервисов выглядят как /external/resources/ModuleName
-         // в таких случаях файлы module-dependencies и bundlesRoute необходимо искать для указанного модуля
-         // необходимо вырезать только название модуля ModuleName и тогда require поймет, что
-         // ModuleName/module-dependencies необходимо загружать из модуля стороннего сервиса
          const moduleParts = modules[name].path.split('/');
          return moduleParts[moduleParts.length - 1];
       });
