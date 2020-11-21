94086c2479a7b32c07234e177a3edc5fd5e7155b
va.ten <va.ten@tensor.ru>
2020-11-21 14:56:38 +0300

    https://online.sbis.ru/opendoc.html?guid=5e4bcecc-3f6c-453c-b374-e49e1703ab76

--- UI/_vdom/Synchronizer/resources/DOMEnvironment.ts
+++ UI/_vdom/Synchronizer/resources/DOMEnvironment.ts
@@ -1028,7 +1028,7 @@ function vdomEventBubbling(
                if (!fn.control._destroyed && (!controlNode || fn.control !== controlNode.control) &&
                      ((eventObject.nativeEvent && fn.control._mounted) || !eventObject.nativeEvent)) {
                   try {
-                     if (!fn.control._mounted && eventObject.type === 'mouseEnter') {
+                     if (!fn.control._mounted && eventObject.type === 'mouseenter') {
                         /* Асинхронный _afterMount контролов приводит к тому,
                          * что события с dom начинают стрелять до маунта,
                          * в таком случае их надо вызвать отложено */
