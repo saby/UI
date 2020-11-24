21993da6bff0b0e03a84ffb7ae1a056958e57aa0
li.mustafin <>
2020-11-24 15:33:36 +0300

    https://online.sbis.ru/opendoc.html?guid=58ed127a-0322-4b63-a2a5-624abba203a9
    https://online.sbis.ru/opendoc.html?guid=02bc8d4f-e774-47b6-986a-9a459b805469

--- UITest/_base/PrefetchLinks.test.ts
+++ UITest/_base/PrefetchLinks.test.ts
@@ -1,12 +1,13 @@
 import { assert } from 'chai';
 import { Head } from 'Application/Page';
 import { PrefetchLinksStore, PrefetchLinksStorePS, handlePrefetchModules } from 'UI/_base/HTML/PrefetchLinks';
+import {JML} from "../../UI/_base/HTML/_meta/interface";
 
 const module = {
     name: 'Module/File',
     path: '/Module/File.js'
 };
-const expectedObject = {"data-vdomignore": true, as: 'script', href: module.path}
+const expectedObject: Record<string, string|boolean> = {"data-vdomignore": true, as: 'script', href: module.path}
 describe('UI/_base/HTML/PrefetchLinks', () => {
     typeof window !== 'undefined' &&
     describe('client side', () => {
@@ -19,7 +20,7 @@ describe('UI/_base/HTML/PrefetchLinks', () => {
             pls.addPrefetchModules([module.name]);
             const data = Head.getInstance().getData();
             assert.equal(data.length, 1);
-            assert.deepEqual(data[0], ['link', {...{rel: 'prefetch'}, ...expectedObject}]);
+            data.forEach((row: JML) => checker.apply(null, row.concat({tag: 'link', rel: 'prefetch', ...expectedObject})));
         });
 
         it('addPreloadModules', () => {
@@ -27,7 +28,7 @@ describe('UI/_base/HTML/PrefetchLinks', () => {
             pls.addPreloadModules([module.name]);
             const data = Head.getInstance().getData();
             assert.equal(data.length, 1);
-            assert.deepEqual(data[0], ['link', {...{rel: 'preload'}, ...expectedObject}]);
+            data.forEach((row: JML) => checker.apply(null, row.concat({tag: 'link', rel: 'preload', ...expectedObject})));
         });
     });
 
@@ -61,7 +62,25 @@ describe('UI/_base/HTML/PrefetchLinks', () => {
             handlePrefetchModules(pageDeps);
             const data = Head.getInstance().getData();
             assert.equal(data.length, 1);
-            assert.deepEqual(data[0], ['link', {...{rel: 'prefetch'}, ...expectedObject}]);
+            data.forEach((row: JML) => checker.apply(null, row.concat({tag: 'link', rel: 'prefetch', ...expectedObject})));
         });
     });
 });
+
+/**
+ * Проверка корректности добавленного тега в Head API
+ * @param tag название тега
+ * @param contents
+ * @param attrs атрибуты тега, который добавили в Head
+ * @param expected эталонные данные
+ */
+function checker(tag, contents, attrs, expected) {
+    if (contents instanceof Object) {
+        expected = attrs;
+        attrs = contents;
+    }
+    assert.equal(tag, expected.tag);
+    assert.equal(attrs.rel, expected.rel);
+    assert.equal(attrs.as, expected.as);
+    assert.include(attrs.href, expected.href);
+}
