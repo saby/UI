56c35eeb9714317a11afb557a0e13f84f0a39c6f
aa.shipin <aa.shipin@tensor.ru>
2020-11-24 03:18:39 +0300

    https://online.sbis.ru/opendoc.html?guid=340ff71c-de50-4f36-9be6-645866dec106

--- UI/_builder/Tmpl/codegen/templates.js
+++ UI/_builder/Tmpl/codegen/templates.js
@@ -222,6 +222,7 @@ define('UI/_builder/Tmpl/codegen/templates', [
       return tmpl
          .replace(/\/\*#PROPERTY_NAME#\*\//g, propertyName)
          .replace(/\/\*#TEMPLATE_BODY#\*\//g, templateBody)
+         .replace(/\/\*#LOCALIZATION_INIT#\*\//g, localizationBlock)
          .replace(/\/\*#LOCALIZATION_INIT#\*\//g, localizationBlock);
    }
 
