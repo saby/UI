4b0ab2158de02bf0d42814deb5475d69ff53d6d6
sv.pecherkin <sv.pecherkin@tensor.ru>
2020-11-30 17:40:17 +0700

    https://online.sbis.ru/opendoc.html?guid=309045f1-ae1d-42eb-82e1-66888166e7bd

--- UI/theme/_controller/css/LinkPS.ts
+++ UI/theme/_controller/css/LinkPS.ts
@@ -1,7 +1,6 @@
 /// <amd-module name='UI/theme/_controller/css/LinkPS' />
 import { default as Link } from './Link';
 import { THEME_TYPE } from './const';
-import { ICssEntity } from './interface';
 
 /**
  * Мультитемная ссылка на СП
