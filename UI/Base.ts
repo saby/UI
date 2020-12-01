c4e4f6095a6ec60d3ca1e009c269bdad191b35d9
mi.hambelov <mi.hambelov@tensor.ru>
2020-12-01 13:46:25 +0300

    https://online.sbis.ru/opendoc.html?guid=5f9d1c53-a31a-4ce8-bcb9-e88d407bf124

--- UI/Base.ts
+++ UI/Base.ts
@@ -11,7 +11,6 @@
 import {default as Control, IControlOptions, TemplateFunction} from './_base/Control';
 import HTML from './_base/HTML';
 import Document from './_base/Document';
-import StateReceiver from './_state/StateReceiver';
 
 import AppData from './_base/AppData';
 import Start from './_base/Start';
@@ -40,7 +39,6 @@ export {
     IControlOptions,
     HTML,
     Document,
-    StateReceiver,
     AppData,
     Start,
     BaseRoute,
