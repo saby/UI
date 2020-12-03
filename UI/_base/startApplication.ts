687217b0748d62f825a4d807feb6dfa1dc3093bd
mi.hambelov <mi.hambelov@tensor.ru>
2020-12-03 12:56:35 +0300

    https://online.sbis.ru/opendoc.html?guid=5cee648e-f949-42b0-a5a4-c8e12e7e5806

--- UI/_base/startApplication.ts
+++ UI/_base/startApplication.ts
@@ -2,6 +2,7 @@
 import { default as AppInit, isInit } from 'Application/Initializer';
 import { StateReceiver } from 'Application/State';
 import { Serializer } from 'UI/State';
+import { Logger } from 'UI/Utils';
 
 /**
  * Инициализация Application/Env для Sbis приложения
@@ -14,7 +15,7 @@ export default function startApplication(cfg?: Record<string, any>) {
 
     let config = cfg || window && window['wsConfig'];
 
-    const stateReceiverInst = new StateReceiver(Serializer);
+    const stateReceiverInst = new StateReceiver(Serializer, Logger);
     AppInit(config, void 0, stateReceiverInst);
 
     if (typeof window !== 'undefined' && window['receivedStates']) {
