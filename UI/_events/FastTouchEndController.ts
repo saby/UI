b504c0e503201491b6ae7d21706024bc845bd82e
va.ten <va.ten@tensor.ru>
2020-12-21 14:32:57 +0300

    https://online.sbis.ru/opendoc.html?guid=38272b18-2678-4fa2-8d0b-734e8c4e35c7

--- UI/_events/FastTouchEndController.ts
+++ UI/_events/FastTouchEndController.ts
@@ -1,43 +1,49 @@
 /**
  * @author Тэн В.А.
  */
+const fastEventList = ['mousedown', 'mouseup', 'click'];
+
 export class FastTouchEndController {
    static clickEmulate(targetElement: Element, nativeEvent: TouchEvent): void {
+      if (!nativeEvent) {
+         return;
+      }
       nativeEvent.preventDefault();
-      const clickEvent = document.createEvent('MouseEvents');
       const touch = nativeEvent.changedTouches[0];
-      clickEvent.initMouseEvent('click',
-         true,
-         true,
-         window,
-         1,
-         touch.screenX,
-         touch.screenY,
-         touch.clientX,
-         touch.clientY,
-         false,
-         false,
-         false,
-         false,
-         0,
-         null);
-      targetElement.dispatchEvent(clickEvent);
-
-      clickEvent.initMouseEvent('mousedown',
-         true,
-         true,
-         window,
-         1,
-         touch.screenX,
-         touch.screenY,
-         touch.clientX,
-         touch.clientY,
-         false,
-         false,
-         false,
-         false,
-         0,
-         null);
-      targetElement.dispatchEvent(clickEvent);
+      let clickEvent;
+      for (let i = 0; i < fastEventList.length; i++) {
+         clickEvent = new MouseEvent(fastEventList[i], this.createMouseEvent(fastEventList[i], nativeEvent, touch));
+         targetElement.dispatchEvent(clickEvent);
+      }
    }
+
+   private static createMouseEvent(eventName: string, event: TouchEvent, touch: Touch) {
+      return {
+         type: eventName,
+         bubbles: event.bubbles,
+         cancelable: event.cancelable,
+         view: window,
+         detail: 1,
+         screenX: touch.screenX,
+         screenY: touch.screenY,
+         clientX: touch.clientX,
+         clientY: touch.clientY,
+         ctrlKey: event.ctrlKey,
+         altKey: event.altKey,
+         shiftKey: event.shiftKey,
+         metaKey: event.metaKey,
+         button: 0,
+         buttons: 0,
+         relatedTarget: null,
+         target: event.target,
+         currentTarget: event.currentTarget,
+         eventPhase: 1, // capture phase
+         stopPropagation(): void {
+            this.bubbles = false;
+         },
+         preventDefault(): void {
+            // no action
+         }}
+   }
+
 }
