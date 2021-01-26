96e94f30911c86d2d92f3ff6a35b85c397c7595d
sv.pecherkin <sv.pecherkin@tensor.ru>
2021-01-26 15:31:16 +0700

    https://online.sbis.ru/opendoc.html?guid=6ed94230-7e04-4f0c-9b50-d14111f4923a

--- UI/_base/HTML.ts
+++ UI/_base/HTML.ts
@@ -54,7 +54,7 @@ class HTML extends Control<IHTMLCombinedOptions> {
     // @ts-ignore
     _template: Function = template;
 
-    private _bodyClasses: string = '';
+    private _bodyClasses: string = '__htmlBodyClasses';
     private onServer: Boolean = false;
     // tslint:disable-next-line:ban-ts-ignore
     // @ts-ignore
@@ -87,7 +87,9 @@ class HTML extends Control<IHTMLCombinedOptions> {
     private initState(cfg: any): void {
         this.templateConfig = cfg.templateConfig;
         this.compat = cfg.compat || false;
-        this._bodyClasses = cfg.bodyClasses || '';
+        if (!this.onServer) {
+            this._bodyClasses = cfg.bodyClasses || '';
+        }
     }
 
     private isFocusNode(node: Element): boolean {
@@ -186,7 +188,6 @@ class HTML extends Control<IHTMLCombinedOptions> {
             return;
         }
         return new Promise((resolve) => {
-            this._bodyClasses = AppBody.getInstance().getClassString();
             resolve({
                 bodyClasses: this._bodyClasses,
                 buildnumber: this.buildnumber,
