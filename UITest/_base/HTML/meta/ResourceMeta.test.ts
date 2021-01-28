b8a901d0b783f4409091d95efe1ba6cbf881b56e
mi.hambelov <mi.hambelov@tensor.ru>
2021-01-28 21:48:14 +0300

    https://online.sbis.ru/opendoc.html?guid=a8bbf07d-0aa6-497c-8634-7d79e365dffe

--- UITest/_base/HTML/meta/ResourceMeta.test.ts
+++ UITest/_base/HTML/meta/ResourceMeta.test.ts
@@ -1,11 +1,12 @@
 import { assert } from 'chai';
 import { getMetaStack, ResourceMeta } from 'UI/_base/HTML/meta';
-
+/* tslint:disable */
 describe('UI/_base/HTML/_meta/ResourceMeta', () => {
     it('проверка метода enter', () => {
         const TITLE_CONTENT = 'ResourceMetaTitle1';
         const resource = new ResourceMeta({title: TITLE_CONTENT});
         resource.enter();
+        // @ts-ignore
         assert.isTrue(getMetaStack().lastState._meta.title === TITLE_CONTENT,
             'В stack не был добавлен актуальный state');
     });
@@ -14,6 +15,7 @@ describe('UI/_base/HTML/_meta/ResourceMeta', () => {
         const resource = new ResourceMeta({title: TITLE_CONTENT});
         resource.enter();
         resource.dispose();
+        // @ts-ignore
         assert.isFalse(getMetaStack().lastState._meta.title === TITLE_CONTENT,
             'В stack не был удален текущий state');
     });
