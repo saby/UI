581978e9b59667753c5832bcc0c8a2e46b7f01f2
ma.krylov <ma.krylov@tensor.ru>
2021-02-01 22:59:54 +0300

    little fix
    https://online.sbis.ru/opendoc.html?guid=d8263fdb-c03e-4f27-8bbb-59db23fb4441

--- UI/_builder/Tmpl/codegen/Internal.ts
+++ UI/_builder/Tmpl/codegen/Internal.ts
@@ -1,13 +1,13 @@
 import { InternalNode, InternalNodeType, IProgramMeta } from "UI/_builder/Tmpl/core/Internal";
 import { ExpressionVisitor, ProgramNode } from "UI/_builder/Tmpl/expressions/_private/Nodes";
 
-const FUNCTION_PREFIX = '__$getInternal_';
+const FUNCTION_PREFIX = '__$calculateDirtyCheckingVars_';
 const INTERNAL_PROGRAM_PREFIX = '__dirtyCheckingVars_';
 const COLLECTION_NAME = 'collection';
-const CONDITIONAL_VARIABLE_NAME = 'tmp';
+const CONDITIONAL_VARIABLE_NAME = 'temp';
 const CONTEXT_VARIABLE_NAME = 'data';
 // FIXME: переменная funcContext неправильно вставлена в генератор кода mustache-выражения
-const FUNCTION_HEAD = `var funcContext = ${CONTEXT_VARIABLE_NAME}; var ${COLLECTION_NAME} = { }; var ${CONDITIONAL_VARIABLE_NAME};`;
+const FUNCTION_HEAD = `var funcContext=${CONTEXT_VARIABLE_NAME};var ${COLLECTION_NAME}={};var ${CONDITIONAL_VARIABLE_NAME};`;
 const FUNCTION_TAIL = `return ${COLLECTION_NAME};`;
 const USE_INTERNAL_FUNCTIONS = true;
 
@@ -19,7 +19,9 @@ export function generate(node: InternalNode, functions: Function[]): string {
    if (isEmpty(node)) {
       return '{}';
    }
-   // TODO: Optimize!!! There we can create duplicate internal function
+   if (node.index === -1) {
+      throw new Error('Произведена попытка генерации Internal-функции от скрытого узла');
+   }
    const functionName = FUNCTION_PREFIX + node.index;
    const body = FUNCTION_HEAD + build(node) + FUNCTION_TAIL;
    const index = node.ref.getCommittedIndex(body);
@@ -51,15 +53,15 @@ function build(node: InternalNode): string {
    if (node.type === InternalNodeType.IF) {
       const test = buildProgram(node.test.node);
       let prefix = wrapProgram(node.test, CONDITIONAL_VARIABLE_NAME);
-      return ` if((${CONDITIONAL_VARIABLE_NAME}=(${test}))){${prefix + body}}`;
+      return `if((${CONDITIONAL_VARIABLE_NAME}=(${test}))){${prefix + body}}`;
    }
    if (node.type === InternalNodeType.ELSE_IF) {
       const test = buildProgram(node.test.node);
       let prefix = wrapProgram(node.test, CONDITIONAL_VARIABLE_NAME);
-      return ` else if((${CONDITIONAL_VARIABLE_NAME}=(${test}))){${prefix + body}}`;
+      return `else if((${CONDITIONAL_VARIABLE_NAME}=(${test}))){${prefix + body}}`;
    }
    if (node.type === InternalNodeType.ELSE && body.length > 0) {
-      return ` else {${body}}`;
+      return `else{${body}}`;
    }
    return body;
 }
@@ -83,7 +85,7 @@ function buildPrograms(programs: IProgramMeta[]): string {
 }
 
 function wrapProgram(meta: IProgramMeta, code: string): string {
-   return `${COLLECTION_NAME}["${INTERNAL_PROGRAM_PREFIX}${meta.index}"] = ${code};`;
+   return `${COLLECTION_NAME}.${INTERNAL_PROGRAM_PREFIX}${meta.index}=${code};`;
 }
 
 function buildMeta(meta: IProgramMeta): string {
