3bf8d799df805f9b9d93926c7744e82eb9d9fdb9
Крылов М.А <ma.krylov@tensor.ru>
2021-02-17 12:39:33 +0300

    https://online.sbis.ru/opendoc.html?guid=dee5a775-1eeb-4c46-825b-9ed18481d7f2

--- UI/_builder/Tmpl/function.js
+++ UI/_builder/Tmpl/function.js
@@ -131,8 +131,9 @@ define('UI/_builder/Tmpl/function', [
                this.functionNames[functionName] = 1;
                return functionName;
             }
-            functionName += ('_' + this.functionNames[functionName].toString());
+            var idx = this.functionNames[functionName].toString();
             this.functionNames[functionName]++;
+            functionName += '_' + idx;
          }
          // Перед возвратом имени убедиться, что не происходит переопределение
          if (this.functionNames.hasOwnProperty(functionName)) {
