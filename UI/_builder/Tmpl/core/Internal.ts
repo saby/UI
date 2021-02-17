8f7587640c78988b67c9405e7e18214ae4e239fc
Крылов М.А <ma.krylov@tensor.ru>
2021-02-17 17:30:52 +0300

    https://online.sbis.ru/opendoc.html?guid=00ff9a64-9051-450a-b9bd-105af54243a4

---
+++ UI/_builder/Tmpl/core/Internal.ts
@@ -0,0 +1,9 @@
+
+/**
+ * Флаг включения/выключения нового механизма формирования internal-выражений для dirty checking проверок.
+ */
+const USE_INTERNAL_MECHANISM = false;
+
+export function isUseNewInternalMechanism(): boolean {
+    return USE_INTERNAL_MECHANISM;
+ }
