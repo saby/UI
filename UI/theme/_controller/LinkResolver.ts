8914026226e36de76cbe238497c8b9a46c994535
Ivan Mogilevsky <mogilevsky.ivan@gmail.com>
2021-03-02 10:44:54 +0300

    https://online.sbis.ru/opendoc.html?guid=d6dc3bbe-50f8-4465-bb9c-c28ee7e1976e

--- UI/theme/_controller/LinkResolver.ts
+++ UI/theme/_controller/LinkResolver.ts
@@ -51,8 +51,10 @@ function createRequireRoutes(): any {
       router: 'router',
 
       jquery: '/cdn/JQuery/jquery/3.3.1/jquery-min',
-      react: 'cdn/React/17.0.1.1/react.production.min',
-      'react-dom': 'cdn/React/17.0.1.1/react-dom.production.min'
+      react: 'UI/_react/third-party/react.production.min',
+      'react-dom': 'UI/_react/third-party/react-dom.production.min',
+      'react/jsx-runtime': 'UI/_react/third-party/react-jsx-runtime.production.min',
+      'react/jsx-dev-runtime': 'UI/_react/third-party/react-jsx-dev-runtime.production.min'
    };
 }
 
