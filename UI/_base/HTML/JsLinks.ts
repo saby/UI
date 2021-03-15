315f5f4eac61c807272aeb356d3332f3befc9d43
no.novolokova <no.novolokova@tensor.ru>
2021-03-15 15:14:16 +0700

    JSlinks наполнение https://online.sbis.ru/opendoc.html?guid=36e8e6eb-30a1-47f9-bd75-f82a6a4837c6

--- UI/_base/HTML/JsLinks.ts
+++ UI/_base/HTML/JsLinks.ts
@@ -8,7 +8,10 @@ import template = require('wml!UI/_base/HTML/JsLinks');
 import { headDataStore } from 'UI/_base/HeadData';
 import { IControlOptions } from 'UI/Base';
 import * as ModulesLoader from 'WasabyLoader/ModulesLoader';
+import { Head as AppHead } from 'Application/Page';
 import { cookie } from "Env/Env";
+import { default as TagMarkup } from 'UI/_base/HTML/_meta/TagMarkup';
+import { fromJML } from 'UI/_base/HTML/_meta/JsonML';
 
 interface IJsLinksOptions extends IControlOptions {
    resourceRoot: string;
@@ -18,49 +21,79 @@ interface IJsLinksOptions extends IControlOptions {
  */
 class JsLinks extends Control<IJsLinksOptions> {
    _template: TemplateFunction = template;
-
-   js: Record<string, number> = {};
-   tmpl: string[] = [];
-   wml: string[] = [];
-   themedCss: string[] = [];
-   simpleCss: string[] = [];
-   rsSerialized: string = '';
-   rtpackModuleNames: string = '';
-
+   jslinksData: String = '';
    _beforeMount(options: IJsLinksOptions): Promise<void> {
       if (typeof window !== 'undefined') {
          return;
       }
       return headDataStore.read('waitAppContent')().then((res) => {
+         let jslinksAPI = AppHead.getInstance();
          const jsLinks: string[] = res.js.map((js) => this.resolveLink(js)).concat(res.scripts);
-         this.js = arrayToObject(jsLinks); // конвертируем в hashmap чтобы избавиться от дублей
-         this.tmpl = res.tmpl;
-         this.wml = res.wml;
-         this.rsSerialized = res.rsSerialized;
+
+         if (this.arrayToObject(jsLinks)) {
+            jsLinks.forEach((link, index) =>
+               jslinksAPI.createTag('script', {
+                  type: 'text/javascript',
+                  src: link,
+                  key: 'scripts_' + index,
+                  defer: 'defer'
+               }, ''));
+         }
+         if (res.tmpl) {
+            res.tmpl.forEach((value, index) =>
+               jslinksAPI.createTag('script', {
+                  type: 'text/javascript',
+                  src: this.resolveLink(value, 'tmpl'),
+                  key: 'scripts_' + index,
+                  defer: 'defer'
+               }, ''));
+         }
+         if (res.wml) {
+            res.tmpl.forEach((value, index) =>
+               jslinksAPI.createTag('script', {
+                  type: 'text/javascript',
+                  src: this.resolveLink(value, 'wml'),
+                  key: 'scripts_' + index,
+                  defer: 'defer'
+               }, ''));
+         }
+
+         if (res.rsSerialized) {
+            jslinksAPI.createTag('script', { type: '' },
+               `window['receivedStates']='${res.rsSerialized}';`);
+         }
          /**
-          * На страницах OnlineSbisRu/CompatibleTemplate зависимости пакуются в rt-пакеты и собираются DepsCollector
-          * Поэтому в глобальной переменной храним имена запакованных в rt-пакет модулей
-          * И игнорируем попытки require (см. WS.Core\ext\requirejs\plugins\preload.js)
-          * https://online.sbis.ru/opendoc.html?guid=348beb13-7b57-4257-b8b8-c5393bee13bd
-          * TODO следует избавится при отказе от rt-паковки
-          */
-         this.rtpackModuleNames = JSON.stringify(arrayToObject(res.rtpackModuleNames));
+         * На страницах OnlineSbisRu/CompatibleTemplate зависимости пакуются в rt-пакеты и собираются DepsCollector
+         * Поэтому в глобальной переменной храним имена запакованных в rt-пакет модулей
+         * И игнорируем попытки require (см. WS.Core\ext\requirejs\plugins\preload.js)
+         * https://online.sbis.ru/opendoc.html?guid=348beb13-7b57-4257-b8b8-c5393bee13bd
+         * TODO следует избавится при отказе от rt-паковки
+         */
+
+         if (JSON.stringify(this.arrayToObject(res.rtpackModuleNames))) {
+            jslinksAPI.createTag('script', { type: '' },
+               `window['rtpackModuleNames']='${res.rtpackModuleNames}';`);
+         }
+         const data = jslinksAPI.getData();
+         if (data && data.length) {
+            this.jslinksData += new TagMarkup(data.map(fromJML), { getResourceUrl: false }).outerHTML;
+         }
+         jslinksAPI.clear();
       });
    }
 
    resolveLink(path: string, type: string = ''): string {
       return ModulesLoader.getModuleUrl(type ? `${type}!${path}` : path, cookie.get('s3debug'));
    }
+
+   arrayToObject(arr: string[]): Record<string, number> {
+      const obj: Record<string, number> = {};
+      let index = 0;
+      for (const key of arr) {
+         obj[key] = index++;
+      }
+      return obj;
+   }
 }
 
 export default JsLinks;
-
-/** Конвертируем в hashmap для быстрого поиска имени модуля */
-function arrayToObject(arr: string[]): Record<string, number> {
-   const obj: Record<string, number> = {};
-   let index = 0;
-   for (const key of arr) {
-      obj[key] = index++;
-   }
-   return obj;
-}
