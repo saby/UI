6de8105e9f460fb3e280eda3055dd70f483fc064
sv.pecherkin <sv.pecherkin@tensor.ru>
2021-03-16 21:24:30 +0700

    https://online.sbis.ru/opendoc.html?guid=92871e5b-1b68-4a8c-812e-b707a32bbb3c

--- UI/_deps/DependenciesPlaceholder.ts
+++ UI/_deps/DependenciesPlaceholder.ts
@@ -1,8 +1,11 @@
 /// <amd-module name="UI/_deps/DependenciesPlaceholder" />
 
 import { ICollectedDeps } from "UI/_base/DepsCollector";
+import { EMPTY_THEME, getThemeController, THEME_TYPE } from "UI/theme/controller";
 import { getResourceUrl } from 'UI/Utils';
+import { headDataStore } from 'UI/Base';
 import { JSLinks as AppJSLinks } from 'Application/Page';
+import { handlePrefetchModules } from 'UI/_base/HTML/PrefetchLinks';
 import { IHTMLOptions } from '../_base/interface/IHTML';
 import { IRootTemplateOptions } from '../_base/interface/IRootTemplate';
 
@@ -40,13 +43,20 @@ function addBaseScripts(cfg: IOptions): void {
    }
 }
 
-function aggregateCSS(cfg: IOptions, deps: ICollectedDeps): void {
-   return;
+export function aggregateCSS(theme: string, styles: string[] = [], themes: string[] = []): Promise<string> {
+   const tc = getThemeController();
+   const gettingStyles = styles.filter((name) => !!name).map((name) => tc.get(name, EMPTY_THEME));
+   const gettingThemes = themes.filter((name) => !!name).map((name) => tc.get(name, theme, THEME_TYPE.SINGLE));
+   return Promise.all(gettingStyles.concat(gettingThemes)).then();
 }
 
 /** Пространство имен для хранения базовых зависимостей страницы. Их важно указывать первыми. */
 export const BASE_DEPS_NAMESPACE: string = 'baseDeps';
 
 export function aggregateDependencies(cfg: IOptions): void {
+   const deps = headDataStore.read('collectDependencies')();
+
+   handlePrefetchModules(deps.js);
    addBaseScripts(cfg);
+   aggregateCSS(cfg.theme, deps.css.simpleCss, deps.css.themedCss);
 }
