f106cb46117a35142ff36fb34854cfb876db6a7d
mi.hambelov <mi.hambelov@tensor.ru>
2021-03-26 16:02:11 +0300

    https://online.sbis.ru/opendoc.html?guid=9e533d05-2023-4e88-9796-0acd17449777

--- UI/Base.ts
+++ UI/Base.ts
@@ -3,22 +3,19 @@
  * @library UI/Base
  * @includes Control UI/_base/Control
  * @includes HTML UI/_base/HTML
- * @includes IMetaStack UI/_base/HTML/_meta/IMetaStack
- * @includes IMetaState UI/_base/HTML/_meta/IMetaState
- * @includes IMeta UI/_base/HTML/_meta/IMeta
- * @includes IOpenGraph UI/_base/HTML/_meta/IOpenGraph
  * @includes Creator UI/_base/Creator
  * @includes Async UI/_async/Async
  * @public
  * @author Шипин А.А.
  */
 import {default as Control, IControlOptions, TemplateFunction, IControlChildren} from './_base/Control';
-export { IMetaStack, IMetaState, IMeta, IOpenGraph } from 'UI/_base/HTML/meta';
+import * as meta from './_base/HTML/meta';
 export * from './_base/CommonLib';
 
 export {
     Control,
     IControlOptions,
     TemplateFunction,
-    IControlChildren
+    IControlChildren,
+    meta
 };
