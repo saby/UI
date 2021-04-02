73338d576f5148d4e3045b5c542b657bffc9a1b1
Ivan Mogilevsky <mogilevsky.ivan@gmail.com>
2021-04-02 17:05:51 +0300

    Заменить createControl на tsx
    https://online.sbis.ru/opendoc.html?guid=5dccbcb7-b9a5-45a6-9e11-271e60eadc51

---
+++ UIDemo/ReactDemo/Reactivity/ClassChild.tsx
@@ -0,0 +1,16 @@
+import {PureComponent, ReactElement} from 'react';
+import {RecordSet} from 'Types/collection';
+
+interface IChildProps {
+    rs: RecordSet;
+}
+
+class ClassChild extends PureComponent<IChildProps> {
+    render(): ReactElement {
+        return <div>Количество записей: ${this.props.rs.getCount()}</div>;
+    }
+}
+
+export {
+    ClassChild
+};
