61550f4b7480958996f1afbfea71698957c19211
rn.kondakov <rn.kondakov@tensor.ru>
2021-04-02 13:13:13 +0300

    Циклическая зависимость https://online.sbis.ru/opendoc.html?guid=2582bd58-4efe-4054-982e-2f12f36f760a

--- UI/_async/Async.ts
+++ UI/_async/Async.ts
@@ -3,7 +3,8 @@ import * as ModulesLoader from 'WasabyLoader/ModulesLoader';
 import * as Library from 'WasabyLoader/Library';
 import { IoC, constants } from 'Env/Env';
 import { descriptor } from 'Types/entity';
-import { Control, IControlOptions, TemplateFunction } from 'UI/Base';
+import { Control, TemplateFunction } from 'UICore/Base';
+import { IControlOptions } from 'UICommon/interfaces';
 import { headDataStore } from 'UI/Deps';
 import template = require('wml!UI/_async/Async');
 
