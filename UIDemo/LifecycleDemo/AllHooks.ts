f7fd89bf7339a79f9b475daeea58e77b98fab112
va.ten <va.ten@tensor.ru>
2021-04-07 13:17:54 +0300

    https://online.sbis.ru/opendoc.html?guid=11d9d383-91c4-4c28-b960-112faffd40d8

--- UIDemo/LifecycleDemo/AllHooks.ts
+++ UIDemo/LifecycleDemo/AllHooks.ts
@@ -8,11 +8,10 @@ export default class AllHooks extends Control {
    protected _componentDidMountCalled: boolean = false;
    protected _afterMountCalled: boolean = false;
    protected _beforeUpdateCalled: boolean = false;
-   protected _componentDidUpdateCalled: boolean = false;
+   protected _afterRenderCalled: boolean = false;
    protected _afterUpdateCalled: boolean = false;
 
    protected _beforePaintCalled: boolean = false;
-   protected _afterRenderCalled: boolean = false;
 
    protected _beforeMount(): void {
       this._beforeMountCalled = true;
@@ -28,10 +27,6 @@ export default class AllHooks extends Control {
 
    protected _beforeUpdate(): void {
       this._beforeUpdateCalled = true;
-   }
-
-   protected _componentDidUpdate(): void {
-      this._componentDidUpdateCalled = true;
    }
 
    protected _afterUpdate(): void {
@@ -51,7 +46,6 @@ export default class AllHooks extends Control {
       this._componentDidMountCalled = false;
       this._afterMountCalled = false;
       this._beforeUpdateCalled = false;
-      this._componentDidUpdateCalled = false;
       this._afterUpdateCalled = false;
       this._beforePaintCalled = false;
       this._afterRenderCalled = false;
