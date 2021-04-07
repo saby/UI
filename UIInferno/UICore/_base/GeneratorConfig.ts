c090970a0e35d63c8f662a41bfe427e31e64ce0b
aa.shipin <aa.shipin@tensor.ru>
2021-04-07 16:43:29 +0300

    https://online.sbis.ru/opendoc.html?guid=b642b7b7-e92d-498c-96b6-220ac27ec535

--- UIInferno/UICore/_base/GeneratorConfig.ts
+++ UIInferno/UICore/_base/GeneratorConfig.ts
@@ -2,11 +2,18 @@
 import { _IGeneratorType } from 'UICore/Executor';
 import { _FocusAttrs } from 'UICore/Focus';
 import { cookie } from 'Env/Env';
+import {isNewEnvironment} from 'UI/Utils';
 
 function prepareAttrsForPartial(attributes: any): void {
    return _FocusAttrs.prepareAttrsForFocus(attributes.attributes);
 }
 function prepareAttrsForRoot(attributes: any, options: any): void {
+   if (isNewEnvironment()) {
+      // заполняем только для старых страниц
+      return;
+   }
+   // временное решение до тех пор пока опция темы не перестанет быть наследуемой
+   // добавлено тут https://online.sbis.ru/opendoc.html?guid=5a70cc3b-0d05-4071-8ba3-3dd6cd1ba0bd
    attributes.attributes.class = attributes.attributes.class || '';
    if (typeof options.theme === 'string') {
       const themeName = 'controls_theme-' + options.theme;
