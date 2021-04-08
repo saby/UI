98096fe0c607759956cfc6a5e00e8c06d79ee837
Aleksander Smirnov <44801232+SparCrow@users.noreply.github.com>
2021-04-08 20:00:42 +0300

    Revert "Документ №1181592124 от 2021-04-02 Шипин А.А."

--- UIReact/UICore/_executor/_Markup/Vdom/Generator.ts
+++ UIReact/UICore/_executor/_Markup/Vdom/Generator.ts
@@ -15,7 +15,7 @@ import {
 } from 'UICommon/Executor';
 import { convertAttributes, WasabyAttributes } from './Attributes';
 import { WasabyContextManager } from 'UICore/Contexts';
-import { IWasabyEvent } from 'UICommon/Events';
+import { IWasabyEvent, IWasabyEventSystem } from 'UICommon/Events';
 import { setEventHook } from 'UICore/Events';
 
 import { Control, TemplateFunction } from 'UICore/Base';
@@ -92,6 +92,7 @@ export class GeneratorVdom implements IGenerator {
       const newOptions = {
          ...resolvedOptionsExtended,
          ...{events},
+         ...{eventSystem: config.data?._options?.eventSystem},
          ref: createChildrenRef(config.viewController, name)
       };
 
@@ -278,6 +279,8 @@ export class GeneratorVdom implements IGenerator {
           {...attrs.events};
       const eventsObject = {
          events: extractedEvents,
+         //@ts-ignore _options объявлен пустым объектом по-умолчанию
+         eventSystem: control._options.eventSystem
       };
       if (!attrToDecorate) {
          attrToDecorate = {};
@@ -316,6 +319,7 @@ function createEventRef<T extends HTMLElement>(
    tagName: string,
    eventsObject: {
       events: Record<string, IWasabyEvent[]>;
+      eventSystem: IWasabyEventSystem;
    },
    prevRef?: React.RefCallback<T>
 ): React.RefCallback<T> {
