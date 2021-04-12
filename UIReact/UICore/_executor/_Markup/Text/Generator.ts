1f30a96165fea6628afce1f7213784ef9b520304
rn.kondakov <rn.kondakov@tensor.ru>
2021-04-12 11:12:24 +0300

    Добавление необходимых для сборки html.tmpl методов текстового генератора https://online.sbis.ru/opendoc.html?guid=79945e05-78e8-490f-bb17-f8f54f095302

--- UIReact/UICore/_executor/_Markup/Text/Generator.ts
+++ UIReact/UICore/_executor/_Markup/Text/Generator.ts
@@ -3,20 +3,45 @@ import {
    CommonUtils,
    IGenerator
 } from 'UICommon/Executor';
+import { IControlOptions } from 'UICommon/interfaces';
 
 /**
  * @author Тэн В.А.
  */
 export class GeneratorText implements IGenerator {
-   createText(value: string): string {
-      return value;
+   createText(text: string): string {
+      return text;
    }
 
-   createDirective(value: string): string {
-      return '<' + value + '>';
+   createDirective(text: string): string {
+      return '<' + text + '>';
    }
 
    escape(value: string): string {
       return CommonUtils.escape(value);
+   }
+
+   createComment(text: string): string {
+      return '<!--' + text + '-->';
+   }
+
+   createTag(tag: string): string {
+      return this.createDirective(tag);
+   }
+
+   getScope(data: any): any {
+      return data;
+   }
+
+   prepareDataForCreate(): IControlOptions {
+      return undefined;
+   }
+
+   createControl(): string {
+      return '<div></div>';
+   }
+
+   createControlNew(): string {
+      return '<div></div>';
    }
 }
