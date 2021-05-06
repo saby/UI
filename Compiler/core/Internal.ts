dd406bacd98830fd3cf73c6072d94de45a1f1567
ma.krylov <ma.krylov@tensor.ru>
2021-05-06 17:03:14 +0300

    https://online.sbis.ru/opendoc.html?guid=2ee658db-ea71-4d67-9ef4-6caece78b4f8

--- Compiler/core/Internal.ts
+++ Compiler/core/Internal.ts
@@ -2,15 +2,6 @@ import * as Ast from './Ast';
 import Scope from './Scope';
 import { IResultTree, InternalVisitor } from './internal/Annotate';
 
-/**
- * Флаг включения/выключения нового механизма формирования internal-выражений для dirty checking проверок.
- */
-const USE_INTERNAL_MECHANISM = true;
-
-export function canUseNewInternalMechanism(): boolean {
-   return USE_INTERNAL_MECHANISM;
-}
-
 export function process(nodes: Ast.Ast[], scope: Scope): IResultTree {
    return new InternalVisitor().process(nodes, scope);
 }
