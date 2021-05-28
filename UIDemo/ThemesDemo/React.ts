fcb5965934b4d5abdd5ddf126915f0d3e0c09ec0
sv.pecherkin <sv.pecherkin@tensor.ru>
2021-05-28 18:02:42 +0700

    https://online.sbis.ru/opendoc.html?guid=bedc0777-1f22-4672-b3cb-57bb87d18379

--- UIDemo/ThemesDemo/React.ts
+++ UIDemo/ThemesDemo/React.ts
@@ -7,4 +7,11 @@ export default class Index extends Control {
    _template: TemplateFunction = template;
    static _theme = ['UIDemo/ThemesDemo/React'];
    static _styles: string[] = ['UIDemo/Index'];
+
+   protected _beforeMount(): Promise<any> | void {
+      return new Promise((resolve) => {
+         // tslint:disable-next-line:no-magic-numbers
+         resolve(42);
+      });
+   }
 }
---
+++ UIReact/ReactUnitTest/_stateReceiver/StateReceiver.test.tsx
@@ -0,0 +1,58 @@
+import { assert } from 'chai';
+import { createSandbox } from 'sinon';
+import { render } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+// FIXME: типы для jsdom нигде не подцеплены, подцепим после переезда на jest
+// tslint:disable-next-line:ban-ts-ignore
+// @ts-ignore
+import { JSDOM } from 'jsdom';
+import { default as Parent } from 'ReactUnitTest/_stateReceiver/Controls/Parent';
+
+describe('UICore/Base:Control ReceivedState', () => {
+   let clock;
+   let sandbox;
+   let container: HTMLDivElement;
+
+   before(() => {
+      const browser = new JSDOM();
+      global.window = browser.window;
+      global.document = window.document;
+   });
+
+   after(() => {
+      delete global.window;
+      delete global.document;
+   });
+
+   beforeEach(() => {
+      sandbox = createSandbox();
+      /*
+      _afterMount и _afterUpdate зовутся в отдельной таске, чтобы браузер мог отрисовать кадр.
+      Чтобы не делать тесты асинхронными, мы просто мокнем таймеры и сами будем управлять временем.
+       */
+      clock = sandbox.useFakeTimers();
+
+      container = document.createElement('div');
+      document.body.appendChild(container);
+   });
+
+   afterEach(() => {
+      clock.restore();
+      sandbox.restore();
+
+      container.remove();
+   });
+
+   it('Потомкам проставляется ключ для StateReceiver', () => {
+      let instance;
+      act(() => {
+         instance = render(
+            <Parent data-key="foo"/>,
+            container
+         );
+      });
+
+      /** Искомый потомок второй по счету а поддереве. Поэтому 0 (корень) _ 1 (второй потомок) */
+      assert.equal(instance.getChildrenKey(), '_0_1_');
+   });
+});
