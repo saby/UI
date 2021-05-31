e45a35ffc8b04d647cbe5a2f87cbe359a49a831f
sv.pecherkin <sv.pecherkin@tensor.ru>
2021-05-31 18:19:52 +0700

    https://online.sbis.ru/opendoc.html?guid=9b308b65-e2dd-476a-86a7-2912c2db32eb

--- UIDemo/Index.ts
+++ UIDemo/Index.ts
@@ -1,5 +1,6 @@
 /// <amd-module name="UIDemo/Index" />
 
+import { Body } from "Application/Page";
 import { Control, TemplateFunction } from 'UI/Base';
 import  { constants } from 'Env/Env';
 import template = require('wml!UIDemo/Index');
@@ -11,6 +12,10 @@ class Index extends Control {
    _template: TemplateFunction = template;
    protected isServerSide: boolean = constants.isServerSide;
    static _styles: string[] = ['UIDemo/Index'];
+
+   protected _beforeMount(options: {theme: string}): void {
+      Body.getInstance().addClass(`controls_theme-${options.theme}`);
+   }
 }
 
 export default Index;
