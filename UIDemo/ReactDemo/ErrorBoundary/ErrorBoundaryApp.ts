f0c920ceb1ae939f528583c69e8265ae7db0a33f
mi.hambelov <mi.hambelov@tensor.ru>
2021-06-03 20:34:18 +0300

    https://online.sbis.ru/opendoc.html?guid=529325b2-51a4-4e9a-a481-fb13b6467499

--- UIDemo/ReactDemo/ErrorBoundary/ErrorBoundaryApp.ts
+++ UIDemo/ReactDemo/ErrorBoundary/ErrorBoundaryApp.ts
@@ -4,7 +4,7 @@ import * as template from "wml!UIDemo/ReactDemo/ErrorBoundary/ErrorBoundaryApp";
 
 export default class ErrorBoundaryApp extends Control {
     // tslint:disable-next-line:no-any
-    constructor(props: {errorViewer: any}, context: any) {
+    constructor(props: any, context: any) {
         super(props, context);
     }
     protected _template: TemplateFunction = template;
--- UIDemo/ReactDemo/Index.ts
+++ UIDemo/ReactDemo/Index.ts
@@ -1,11 +1,9 @@
 import { Control, TemplateFunction } from 'UI/Base';
 // @ts-ignore
 import * as template from 'wml!UIDemo/ReactDemo/Index';
-import { AsyncErrorViewer } from 'UICore/Base';
 
 export default class Index extends Control {
    protected _template: TemplateFunction = template;
-   protected asyncErrorViewer: AsyncErrorViewer = AsyncErrorViewer;
    protected clickMe() {
       console.log('was clicked');
    }
